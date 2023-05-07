6b249c7054035e216c2e8b1b4b74c4ab5cec3e2f
cboseb <virtual.user@gmail.com>
2023-05-07 17:39:51 -0400

    We should work on ai git comment writer assisted soon :p

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel
 import uvicorn
 from dotenv import load_dotenv
 import os
-import redis
 
 # Load environment variables
 load_dotenv()
@@ -13,19 +12,8 @@ app = FastAPI()
 # List of allowed TradingView IPs
 TRADINGVIEW_IPS = ["52.89.214.238", "34.212.75.30", "54.218.53.128", "52.32.178.7"]
 
-# Retrieve Redis connection parameters from environment variables
-redis_host = os.getenv("REDIS_HOST")
-redis_port = os.getenv("REDIS_PORT")
-redis_db = os.getenv("REDIS_DB")
-redis_password = os.getenv("REDIS_PASSWORD")
-
-# Initialize Redis client and verify connection
-try:
-    redis_client = redis.Redis(host=redis_host, port=int(redis_port), db=int(redis_db), password=redis_password)
-    if redis_client.ping():
-        print(f"Connected to Redis server at {redis_host}:{redis_port}")
-except Exception as e:
-    print(f"Failed to connect to Redis server: {e}")
+# Initialize global variable for storing the last signal
+last_signal = None
 
 @app.get("/")
 def read_root():
@@ -33,27 +21,28 @@ def read_root():
 
 @app.post("/webhook")
 def webhook(request: Request):
+    global last_signal
     client_host = request.client.host
     if client_host not in TRADINGVIEW_IPS:
         raise HTTPException(status_code=403, detail="Access denied")
-    # Store the signal in Redis
+    # Store the signal in the global variable
     try:
-        redis_client.set('last_signal', request.json())
+        last_signal = request.json()
         return {"status": "ok"}
     except Exception as e:
-        print(f"Failed to store signal in Redis: {e}")
+        print(f"Failed to store signal: {e}")
         raise HTTPException(status_code=500, detail="An error occurred while storing the signal")
 
 @app.get("/viewsignal")
 def view_signal():
+    global last_signal
     try:
-        last_signal = redis_client.get('last_signal')
         if last_signal:
-            return {"signal": last_signal.decode('utf-8')}
+            return {"signal": last_signal}
         else:
             return {"signal": "No signal"}
     except Exception as e:
-        print(f"Failed to retrieve signal from Redis: {str(e)}")
+        print(f"Failed to retrieve signal: {str(e)}")
         raise HTTPException(status_code=500, detail="An error occurred while retrieving the signal")
 
 
