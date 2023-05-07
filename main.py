4c376151cd8f090147fdbc338d512bc591af33b7
cboseb <virtual.user@gmail.com>
2023-05-07 15:54:01 -0400

    We should work on ai git comment writer assisted soon :p

--- main.py
+++ main.py
@@ -1,54 +1,55 @@
 from fastapi import FastAPI, Request, HTTPException
-from pydantic import BaseModel
 from typing import Optional
+from pydantic import BaseModel
 import uvicorn
 import os
 import redis
 from dotenv import load_dotenv
 
-load_dotenv()  # take environment variables from .env.
+load_dotenv()
 
 app = FastAPI()
 
 TRADINGVIEW_IPS = ["52.89.214.238", "34.212.75.30", "54.218.53.128", "52.32.178.7"]
 
-# Load environment variables
-REDIS_HOST = os.getenv("REDIS_HOST")
-REDIS_PORT = os.getenv("REDIS_PORT")
-REDIS_DB = os.getenv("REDIS_DB")
-REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
+class Item(BaseModel):
+    item_id: int
+    q: Optional[str] = None
+
+# Load Redis configuration from environment variables
+redis_host = os.getenv("REDIS_HOST")
+redis_port = os.getenv("REDIS_PORT")
+redis_db = os.getenv("REDIS_DB")
+redis_password = os.getenv("REDIS_PASSWORD")
 
 # Initialize Redis client
-redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, password=REDIS_PASSWORD)
+redis_client = redis.Redis(host=redis_host, port=redis_port, db=redis_db, password=redis_password)
 
-class TradingViewSignal(BaseModel):
-    ticker: str
-    exchange: str
-    price: float
-    time: str
-    # Add more fields based on the payload from TradingView
+def test_redis_connection():
+    try:
+        # Ping the Redis server to test the connection
+        redis_client.ping()
+        print("Connected to Redis")
+        return True
+    except redis.ConnectionError:
+        print("Could not connect to Redis")
+        return False
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
 @app.post("/webhook")
-async def read_webhook_signal(signal: TradingViewSignal, request: Request, x_secret: Optional[str] = None):
+def webhook(request: Request):
     client_host = request.client.host
     if client_host not in TRADINGVIEW_IPS:
         raise HTTPException(status_code=403, detail="Access denied")
-
     # Store the signal in Redis
-    redis_client.set('last_signal', signal.json())
-
+    redis_client.set('last_signal', request.json())
     return {"status": "ok"}
 
-@app.get("/viewsignal")
-async def view_signal():
-    last_signal = redis_client.get('last_signal')
-    if last_signal is not None:
-        last_signal = last_signal.decode('utf-8')
-    return {"last_signal": last_signal}
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+@app.get("/viewsignal
