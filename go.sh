5d3ba95c35336845334d31e110a19d929f221f74
cboseb <virtual.user@gmail.com>
2023-05-07 15:32:19 -0400

    We should work on ai git comment writer assisted soon :p

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git add .
-git commit -m "I will work on ai git comment writer assisted soon :p "
+git commit -m "We should work on ai git comment writer assisted soon :p "
 git push
 eb deploy
