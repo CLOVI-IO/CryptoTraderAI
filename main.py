bc2d69b300999aae8be92d191e06ef4fe9eafc50
cboseb <virtual.user@gmail.com>
2023-05-07 11:15:09 -0400

    receive tradingview alert

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from typing import Union
+from fastapi import FastAPI, Request
+import os
+from dotenv import load_dotenv
 
-from fastapi import FastAPI
+# Load environment variables from .env file
+load_dotenv()
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+#@app.get("/items/{item_id}")
+#def read_item(item_id: int, q: Union[str, None] = None):
+#    return {"item_id": item_id, "q": q}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/tradingview-webhook/")
+async def tradingview_webhook(request: Request):
+    data = await request.json()
+    print(data)  # print the data to the console for now
+    return {"message": "Webhook received"}
