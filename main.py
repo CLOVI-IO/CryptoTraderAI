e3f5875666754583717e6ca3b37fb8e4d6b10b1b
cboseb <virtual.user@gmail.com>
2023-05-07 12:41:18 -0400

    global variable added to remember last signal

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ load_dotenv()
 
 app = FastAPI()
 
-# TRADINGVIEW_IPS = ["52.89.214.238", "34.212.75.30", "54.218.53.128", "52.32.178.7"]
+TRADINGVIEW_IPS = ["52.89.214.238", "34.212.75.30", "54.218.53.128", "52.32.178.7"]
 
 class Item(BaseModel):
     item_id: int
     q: Optional[str] = None
 
-# Global variable to store the most recent signal
-last_signal = {"signal": "No signal"}
+# Initialize the last_signal as an empty dictionary
+last_signal = {}
 
 @app.get("/")
 def read_root():
@@ -29,19 +29,20 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 @app.post("/webhook")
-def webhook(request: Request):
+async def webhook(request: Request):
     # client_host = request.client.host
     # if client_host not in TRADINGVIEW_IPS:
     #     raise HTTPException(status_code=403, detail="Access denied")
     
     # Update the global variable with the new signal
     global last_signal
-    last_signal = request.json()
+    last_signal = await request.json()
     return {"status": "ok"}
 
 @app.get("/viewsignal")
 def view_signal():
-    # Return the most recent signal
+    # Here you would typically fetch the signal from your database or other data source
+    # Now it returns the last signal received
     return last_signal
 
 if __name__ == "__main__":
