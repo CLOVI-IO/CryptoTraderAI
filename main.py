42f445f223649686914a8d0d2b8aefbab0cbdbb5
cboseb <virtual.user@gmail.com>
2023-05-13 17:27:53 -0400

    Should work on Git Comment AI assisted

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 from fastapi import FastAPI, Request, HTTPException
 from pydantic import BaseModel
-import uvicorn
 from dotenv import load_dotenv
 import os
 import json
+import uvicorn
 
+# Load environment variables
 load_dotenv()
 
 app = FastAPI()
 
-TRADINGVIEW_IPS = ["52.89.214.238", "34.212.75.30", "54.218.53.128", "52.32.178.7"]
-
+# Global variable to store the last signal
 last_signal = None
 
 class Signal(BaseModel):
     symbol: str
-    close: str
-    volume: str
+    close: float
+    volume: float
     interval: str
     strategy: str
 
+class Orders(BaseModel):
+    take_profit: float
+    stop_loss: float
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World!"}
 
-@app.get("/webhook")
+@app.post("/webhook")
 async def webhook(request: Request):
-    client_host = request.client.host
-    if client_host not in TRADINGVIEW_IPS:
-        raise HTTPException(status_code=403, detail="Access denied")
     try:
         global last_signal
         last_signal = await request.json()
@@ -51,30 +52,32 @@ def view_signal():
         print(f"Failed to retrieve signal: {str(e)}")
         raise HTTPException(status_code=500, detail="An error occurred while retrieving the signal")
 
-@app.post("/order")
-def create_order(signal: Signal):
-    global last_signal
-    last_signal = signal
-    formatted_output = {
-        "symbol": last_signal.symbol,
-        "close": last_signal.close,
-        "volume": last_signal.volume,
-        "interval": last_signal.interval,
-        "strategy": last_signal.strategy,
-        "take_profit": calculate_take_profit(last_signal),
-        "stop_loss": calculate_stop_loss(last_signal)
-    }
-    return formatted_output
+@app.get("/orders/{symbol}")
+def get_orders(symbol: str):
+    try:
+        # Perform logic to calculate take-profit and stop-loss orders based on the symbol and Crypto.com exchange
+        # Replace the following placeholder code with your actual implementation
+        take_profit = calculate_take_profit(symbol)
+        stop_loss = calculate_stop_loss(symbol)
+        
+        orders = Orders(take_profit=take_profit, stop_loss=stop_loss)
+        
+        return orders.dict()
+    except Exception as e:
+        print(f"Failed to calculate orders: {str(e)}")
+        raise HTTPException(status_code=500, detail="An error occurred while calculating orders")
 
-def calculate_take_profit(signal: Signal):
-    # Implement your logic to calculate the take-profit value
-    # based on the signal information
-    return "<calculated_take_profit>"
+def calculate_take_profit(symbol: str) -> float:
+    # Implement your logic to calculate the take-profit based on the symbol and Crypto.com exchange
+    # You can use an API or library to fetch the necessary data and calculate the take-profit price
+    # Replace the placeholder code below with your actual implementation
+    return 1.03 * last_signal['close']  # 3% take-profit
 
-def calculate_stop_loss(signal: Signal):
-    # Implement your logic to calculate the stop-loss value
-    # based on the signal information
-    return "<calculated_stop_loss>"
+def calculate_stop_loss(symbol: str) -> float:
+    # Implement your logic to calculate the stop-loss based on the symbol and Crypto.com exchange
+    # You can use an API or library to fetch the necessary data and calculate the stop-loss price
+    # Replace the placeholder code below with your actual implementation
+    return 0.97 * last_signal['close']  # 3% stop-loss
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
