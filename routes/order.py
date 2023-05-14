cb18bf0d3afd74c4eef998c43ad941da5358d018
cboseb <virtual.user@gmail.com>
2023-05-14 10:38:35 -0400

    Should work on Git Comment AI assisted

--- routes/order.py
+++ routes/order.py
@@ -1,16 +1,8 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
-from dotenv import load_dotenv
-import os
 from shared_state import state  # Import the shared state
 
-# Load environment variables
-load_dotenv()
-
 router = APIRouter()
-
-risk_reward_ratio = float(os.getenv("RISK_REWARD_RATIO", "0"))
-risk_percentage = float(os.getenv("RISK_PERCENTAGE", "0"))
 
 
 class Signal(BaseModel):
@@ -21,55 +13,31 @@ class Signal(BaseModel):
     strategy: str
 
 
-@router.post("/order")
-def create_order(signal: Signal):
-    state["last_signal"] = signal.dict()
-
-    if state["last_signal"] is None:
-        raise HTTPException(status_code=400, detail="No signal available")
-
-    formatted_output = format_order(state["last_signal"])
-    return formatted_output
-
-
 @router.get("/order")
 def get_order():
-    if state["last_signal"] is None:
+    # Check if last_signal has been set in the shared state
+    if "last_signal" not in state or state["last_signal"] is None:
         raise HTTPException(status_code=400, detail="No signal available")
 
-    formatted_output = format_order(state["last_signal"])
-    return formatted_output
+    # Retrieve the last signal from the shared state
+    last_signal = state["last_signal"]
 
-
-def calculate_take_profit(entry_price):
-    take_profit = entry_price + (entry_price * risk_reward_ratio)
-    return round(take_profit, 2)
-
-
-def calculate_stop_loss(entry_price):
-    stop_loss = entry_price - (entry_price * risk_percentage)
-    return round(stop_loss, 2)
-
-
-def format_order(signal):
-    # Calculate take-profit and stop-loss levels
-    take_profit = calculate_take_profit(signal["close"])
-    stop_loss = calculate_stop_loss(signal["close"])
+    print(f"Retrieved signal from shared state: {last_signal}")  # Debug print
 
     # Format JSON output
     formatted_output = {
-        "symbol": signal["symbol"],
-        "close": signal["close"],
-        "volume": signal["volume"],
-        "interval": signal["interval"],
-        "strategy": signal["strategy"],
-        "take_profit": take_profit,
-        "stop_loss": stop_loss,
+        "symbol": last_signal["symbol"],
+        "close": last_signal["close"],
+        "volume": last_signal["volume"],
+        "interval": last_signal["interval"],
+        "strategy": last_signal["strategy"],
         # Add other tags for the Crypto.com exchange API
         "type": "LIMIT",
         "side": "BUY",
-        "price": signal["close"],
-        "quantity": signal["volume"],
+        "price": last_signal["close"],
+        "quantity": last_signal["volume"],
     }
+
+    print(f"Formatted order: {formatted_output}")  # Debug print
 
     return formatted_output
