4883b52ffebe3709b4fca468a72334b412035917
cboseb <virtual.user@gmail.com>
2023-05-14 17:53:40 -0400

    Code Update

--- exchanges/crypto_com.py
+++ exchanges/crypto_com.py
@@ -1,72 +1,76 @@
-import os
+import time
 import hmac
 import hashlib
-import time
-import json
 import asyncio
 import websockets
+import json
+import os
+import logging
 from dotenv import load_dotenv
+
+load_dotenv()
+logging.basicConfig(level=logging.DEBUG)
 
 
 class CryptoCom:
-    nonce_counter = 1  # Start a counter for nonce
-
     def __init__(self):
-        load_dotenv()
         self.api_key = os.getenv("CRYPTO_COM_API_KEY")
         self.api_secret = os.getenv("CRYPTO_COM_API_SECRET")
-        environment = os.getenv("ENVIRONMENT")
-
-        if environment == "PRODUCTION":
-            self.websocket_url = os.getenv("PRODUCTION_USER_API_WEBSOCKET")
-        else:  # Default to sandbox if ENVIRONMENT is not set to PRODUCTION
-            self.websocket_url = os.getenv("SANDBOX_USER_API_WEBSOCKET")
-
-        self.connection = None
+        self.api_secret = bytes(self.api_secret, "utf-8")
+        self.environment = os.getenv("ENVIRONMENT")
+        self.websocket_url = (
+            os.getenv("PRODUCTION_USER_API_WEBSOCKET")
+            if self.environment == "PRODUCTION"
+            else os.getenv("SANDBOX_USER_API_WEBSOCKET")
+        )
 
     async def connect(self):
-        self.connection = await websockets.connect(self.websocket_url)
-        if self.connection.open:
-            print("Connection established.")
-            await self.authenticate()
+        async with websockets.connect(
+            self.websocket_url, ping_interval=None
+        ) as websocket:
+            logging.debug("Connection to {} established.".format(self.websocket_url))
 
-    async def authenticate(self):
-        nonce = int(time.time() * 1000) + self.nonce_counter  # Increment the nonce
-        CryptoCom.nonce_counter += 1  # Increment the counter
+            # Prepare the authentication message
+            nonce = int(time.time() * 1000)  # Use only timestamp for nonce
+            params = {"api_key": self.api_key, "nonce": nonce}
 
-        signature = hmac.new(
-            bytes(self.api_secret, "utf-8"),
-            msg=f"{self.api_key}{nonce}".encode("utf-8"),
-            digestmod=hashlib.sha256,
-        ).hexdigest()
+            # Sort the keys and combine them in key-value pairs
+            param_string = "".join([f"{k}{v}" for k, v in sorted(params.items())])
 
-        auth_payload = {
-            "method": "public/auth",
-            "params": {"api_key": self.api_key, "nonce": nonce, "sig": signature},
-            "id": 1,
-        }
-        await self.connection.send(json.dumps(auth_payload))
-        response = await self.connection.recv()
-        data = json.loads(response)
-        if data.get("result") == "ok":
-            print("Authentication successful.")
-        else:
-            print("Authentication failed. Please check your API key and secret.")
+            message_string = (
+                "public/auth" + str(nonce) + self.api_key + param_string + str(nonce)
+            )
 
-    async def subscribe(self, channel):
-        subscription_payload = {
-            "method": "subscribe",
-            "params": {"channels": [channel]},
-            "id": 2,
-        }
-        await self.connection.send(json.dumps(subscription_payload))
-        response = await self.connection.recv()
-        print(f"Response to subscription request: {response}")
+            params["sig"] = hmac.new(
+                self.api_secret,
+                msg=message_string.encode("utf-8"),
+                digestmod=hashlib.sha256,
+            ).hexdigest()
 
-    async def run(self):
-        await self.connect()
+            message = {"id": nonce, "method": "public/auth", "params": params}
+            await websocket.send(json.dumps(message))
+
+            response = await websocket.recv()
+            response = json.loads(response)
+            if "code" in response and response["code"] == 10007:
+                logging.error(
+                    "Authentication failed for API key {}. Please check your API key and secret. Response: {}".format(
+                        self.api_key, response
+                    )
+                )
+                return
+
+            while True:
+                try:
+                    response = await websocket.recv()
+                    response = json.loads(response)
+                    logging.debug(response)
+
+                except websockets.exceptions.ConnectionClosed:
+                    logging.error("Connection closed")
+                    break
 
 
 if __name__ == "__main__":
-    crypto_com = CryptoCom()
-    asyncio.get_event_loop().run_until_complete(crypto_com.run())
+    cryptocom = CryptoCom()
+    asyncio.get_event_loop().run_until_complete(cryptocom.connect())
