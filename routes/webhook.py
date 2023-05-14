840568879191c505dd1a04d2272b9bd2c8017a66
cboseb <virtual.user@gmail.com>
2023-05-14 11:17:26 -0400

    Should work on Git Comment AI assisted

--- routes/webhook.py
+++ routes/webhook.py
@@ -1,46 +1,50 @@
-# webhook.py
-from fastapi import APIRouter, Request, HTTPException
-from dotenv import load_dotenv
-import os
-import json
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
 from shared_state import state  # Import the shared state
+import traceback  # import traceback module for detailed error logging
 
 router = APIRouter()
 
-load_dotenv()  # Load environment variables from .env file
+
+class Signal(BaseModel):
+    symbol: str
+    close: float
+    volume: float
+    interval: str
+    strategy: str
 
 
-@router.post("/webhook")
-async def webhook(request: Request):
-    client_host = request.client.host
-    print(f"Client host: {client_host}")  # Debug print
-
-    tradingview_ips = os.getenv("TRADINGVIEW_IPS", "").split(",")
-    print(f"TradingView IPs: {tradingview_ips}")  # Debug print
-
-    if client_host not in tradingview_ips:
-        raise HTTPException(status_code=403, detail="Access denied")
-
+@router.get("/order")
+def get_order():
     try:
-        content_type = request.headers.get("content-type", "")
-        print(f"Content type: {content_type}")  # Debug print
+        # Check if last_signal has been set in the shared state
+        if "last_signal" not in state or state["last_signal"] is None:
+            raise HTTPException(status_code=400, detail="No signal available")
 
-        if "application/json" in content_type:
-            payload = await request.json()
-        elif "text/plain" in content_type:
-            payload = await request.text()
-            payload = json.loads(payload)  # Convert text payload to JSON
-        else:
-            raise HTTPException(status_code=415, detail="Unsupported media type")
+        # Retrieve the last signal from the shared state. This signal was stored by the webhook endpoint.
+        last_signal = state["last_signal"]
 
-        # Process the payload or store it as required
-        state["last_signal"] = payload  # Update the shared state
-        print(f"Received signal: {state['last_signal']}")
+        print(f"Retrieved signal from shared state: {last_signal}")  # Debug print
 
-        return {"status": "ok"}
+        # Format JSON output
+        formatted_output = {
+            "symbol": last_signal.get("symbol", "N/A"),
+            "close": last_signal.get("close", 0.0),
+            "volume": last_signal.get("volume", 0.0),
+            "interval": last_signal.get("interval", "N/A"),
+            "strategy": last_signal.get("strategy", "N/A"),
+            # Add other tags for the Crypto.com exchange API
+            "type": "LIMIT",
+            "side": "BUY",
+            "price": last_signal.get("close", 0.0),
+            "quantity": last_signal.get("volume", 0.0),
+        }
+
+        print(f"Formatted order: {formatted_output}")  # Debug print
+
+        return formatted_output
 
     except Exception as e:
-        print(f"Failed to store signal: {e}")
-        raise HTTPException(
-            status_code=500, detail="An error occurred while storing the signal"
-        )
+        # print the traceback of the error
+        print(f"Failed to create order. Traceback: {traceback.format_exc()}")
+        raise HTTPException(status_code=500, detail=f"Failed to create order: {str(e)}")
