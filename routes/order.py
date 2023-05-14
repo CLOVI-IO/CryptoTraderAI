afebd1d2b9ef3591c06ab7aac125332f7f4dca1a
cboseb <virtual.user@gmail.com>
2023-05-14 10:53:15 -0400

    Should work on Git Comment AI assisted

--- routes/order.py
+++ routes/order.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from shared_state import state  # Import the shared state
+import traceback  # import traceback module for detailed error logging
 
 router = APIRouter()
 
@@ -15,29 +16,35 @@ class Signal(BaseModel):
 
 @router.get("/order")
 def get_order():
-    # Check if last_signal has been set in the shared state
-    if "last_signal" not in state or state["last_signal"] is None:
-        raise HTTPException(status_code=400, detail="No signal available")
+    try:
+        # Check if last_signal has been set in the shared state
+        if "last_signal" not in state or state["last_signal"] is None:
+            raise HTTPException(status_code=400, detail="No signal available")
 
-    # Retrieve the last signal from the shared state
-    last_signal = state["last_signal"]
+        # Retrieve the last signal from the shared state
+        last_signal = state["last_signal"]
 
-    print(f"Retrieved signal from shared state: {last_signal}")  # Debug print
+        print(f"Retrieved signal from shared state: {last_signal}")  # Debug print
 
-    # Format JSON output
-    formatted_output = {
-        "symbol": last_signal["symbol"],
-        "close": last_signal["close"],
-        "volume": last_signal["volume"],
-        "interval": last_signal["interval"],
-        "strategy": last_signal["strategy"],
-        # Add other tags for the Crypto.com exchange API
-        "type": "LIMIT",
-        "side": "BUY",
-        "price": last_signal["close"],
-        "quantity": last_signal["volume"],
-    }
+        # Format JSON output
+        formatted_output = {
+            "symbol": last_signal["symbol"],
+            "close": last_signal["close"],
+            "volume": last_signal["volume"],
+            "interval": last_signal["interval"],
+            "strategy": last_signal["strategy"],
+            # Add other tags for the Crypto.com exchange API
+            "type": "LIMIT",
+            "side": "BUY",
+            "price": last_signal["close"],
+            "quantity": last_signal["volume"],
+        }
 
-    print(f"Formatted order: {formatted_output}")  # Debug print
+        print(f"Formatted order: {formatted_output}")  # Debug print
 
-    return formatted_output
+        return formatted_output
+
+    except Exception as e:
+        # print the traceback of the error
+        print(f"Failed to create order. Traceback: {traceback.format_exc()}")
+        raise HTTPException(status_code=500, detail=f"Failed to create order: {str(e)}")
