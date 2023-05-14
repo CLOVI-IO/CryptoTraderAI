01d0912ad83144e9d1ded4b8d3edc1241e39429a
cboseb <virtual.user@gmail.com>
2023-05-14 16:17:57 -0400

    Code Update

--- exchanges/crypto_com.py
+++ exchanges/crypto_com.py
@@ -1,75 +1,68 @@
-import asyncio
-import json
-import time
+import os
 import hmac
 import hashlib
-from dotenv import load_dotenv
-import os
+import time
+import json
+import asyncio
 import websockets
-
-load_dotenv()
-
-# Fetch environment
-environment = os.getenv("ENVIRONMENT")
-
-# Fetch API keys
-api_key = os.getenv("API_KEY")
-api_secret = os.getenv("API_SECRET")
-
-# Set WebSocket URLs based on environment
-if environment == "PRODUCTION":
-    user_api_websocket_url = os.getenv("PRODUCTION_USER_API_WEBSOCKET")
-else:  # Default to sandbox for safety
-    user_api_websocket_url = os.getenv("SANDBOX_USER_API_WEBSOCKET")
+from dotenv import load_dotenv
 
 
 class CryptoCom:
-    def __init__(self, api_key, api_secret, url):
-        self.api_key = api_key
-        self.api_secret = api_secret
-        self.url = url
+    def __init__(self):
+        load_dotenv()
+        self.api_key = os.getenv("CRYPTO_COM_API_KEY")
+        self.api_secret = os.getenv("CRYPTO_COM_API_SECRET")
+        environment = os.getenv("ENVIRONMENT")
+
+        if environment == "PRODUCTION":
+            self.websocket_url = os.getenv("PRODUCTION_USER_API_WEBSOCKET")
+        else:  # Default to sandbox if ENVIRONMENT is not set to PRODUCTION
+            self.websocket_url = os.getenv("SANDBOX_USER_API_WEBSOCKET")
+
+        self.connection = None
 
     async def connect(self):
-        self.websocket = await websockets.connect(self.url)
-        await self.authenticate()
+        self.connection = await websockets.connect(self.websocket_url)
+        if self.connection.open:
+            print("Connection established.")
+            await self.authenticate()
 
     async def authenticate(self):
         nonce = int(time.time() * 1000)
-        sig_payload = "GET/users/self/verify" + str(nonce)
         signature = hmac.new(
             bytes(self.api_secret, "utf-8"),
-            msg=bytes(sig_payload, "utf-8"),
+            msg=f"{self.api_key}{nonce}".encode("utf-8"),
             digestmod=hashlib.sha256,
         ).hexdigest()
-        await self.websocket.send(
-            json.dumps(
-                {
-                    "id": 1,
-                    "method": "public/auth",
-                    "params": {
-                        "api_key": self.api_key,
-                        "nonce": nonce,
-                        "sig": signature,
-                    },
-                }
-            )
-        )
-        response = await self.websocket.recv()
-        print(f"Authentication response: {response}")
 
-    async def keep_alive(self):
-        while True:
-            await self.websocket.send(json.dumps({"method": "public/ping", "id": 2}))
-            response = await self.websocket.recv()
-            print(f"Keep alive response: {response}")
-            await asyncio.sleep(30)
+        auth_payload = {
+            "method": "public/auth",
+            "params": {"api_key": self.api_key, "nonce": nonce, "sig": signature},
+            "id": 1,
+        }
+        await self.connection.send(json.dumps(auth_payload))
+        response = await self.connection.recv()
+        data = json.loads(response)
+        if data.get("result") == "ok":
+            print("Authentication successful.")
+        else:
+            print("Authentication failed. Please check your API key and secret.")
+
+    async def subscribe(self, channel):
+        subscription_payload = {
+            "method": "subscribe",
+            "params": {"channels": [channel]},
+            "id": 2,
+        }
+        await self.connection.send(json.dumps(subscription_payload))
+        response = await self.connection.recv()
+        print(f"Response to subscription request: {response}")
 
     async def run(self):
         await self.connect()
-        await self.keep_alive()
 
 
-crypto_com = CryptoCom(api_key, api_secret, user_api_websocket_url)
-
-# Run the client
-asyncio.get_event_loop().run_until_complete(crypto_com.run())
+if __name__ == "__main__":
+    crypto_com = CryptoCom()
+    asyncio.get_event_loop().run_until_complete(crypto_com.run())
