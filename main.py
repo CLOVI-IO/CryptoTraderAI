7598d37afd22267c64a0ad6257718519777912bc
cboseb <virtual.user@gmail.com>
2023-05-13 22:22:18 -0400

    Should work on Git Comment AI assisted

--- main.py
+++ main.py
@@ -1,49 +1,26 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from dotenv import load_dotenv
+import os
 import uvicorn
-from routes import webhook, viewsignal
-from shared import SharedState
+
+from routes import webhook
 
 # Load environment variables
 load_dotenv()
 
 app = FastAPI()
-shared_state = SharedState()
 
-
-@app.on_event("startup")
-async def startup_event():
-    # Any startup logic, if needed
-    pass
-
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    # Any shutdown logic, if needed
-    pass
+# Initialize last_signal in the application state
+app.state.last_signal = None
 
 
 @app.get("/")
-def hello_world(shared: SharedState = Depends()):
+def hello_world():
     return {"message": "Hello, World!"}
-
-
-# Use shared state in other routes or endpoints
-@app.post("/update_signal")
-def update_signal(signal, shared: SharedState = Depends()):
-    shared.update_last_signal(signal)
-    return {"message": "Signal updated"}
-
-
-@app.get("/get_last_signal")
-def get_last_signal(shared: SharedState = Depends()):
-    last_signal = shared.get_last_signal()
-    return {"last_signal": last_signal}
 
 
 # Include the route endpoints from other files
 app.include_router(webhook.router)
-app.include_router(viewsignal.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- routes/viewsignal.py
+++ routes/viewsignal.py
@@ -1,15 +1,9 @@
 from fastapi import APIRouter
-from main import app
 
 router = APIRouter()
 
-class SignalResponse:
-    def __init__(self, signal_id: int, signal_name: str):
-        self.signal_id = signal_id
-        self.signal_name = signal_name
 
-@router.get("/viewsignal", response_model=SignalResponse)
+@router.get("/viewsignal")
 def view_signal():
-    last_signal = app.state.last_signal  # Access the global last_signal variable
-    signal = SignalResponse(signal_id=1, signal_name=last_signal)
-    return signal
+    # Handle view signal logic
+    return {"message": "View Signal"}
