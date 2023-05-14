196ca9ff6f5d7b96b220a4fe44585c545b65f2a9
cboseb <virtual.user@gmail.com>
2023-05-14 10:32:29 -0400

    Should work on Git Comment AI assisted

--- routes/order.py
+++ routes/order.py
@@ -1,4 +1,3 @@
-# order.py
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from dotenv import load_dotenv
@@ -10,7 +9,6 @@ load_dotenv()
 
 router = APIRouter()
 
-# Risk management settings
 risk_reward_ratio = float(os.getenv("RISK_REWARD_RATIO", "0"))
 risk_percentage = float(os.getenv("RISK_PERCENTAGE", "0"))
 
@@ -23,33 +21,24 @@ class Signal(BaseModel):
     strategy: str
 
 
-@router.post("/order", response_model=Signal)
+@router.post("/order")
 def create_order(signal: Signal):
-    last_signal = state.get("last_signal")  # Use shared state
-    if last_signal is None:
+    state["last_signal"] = signal.dict()
+
+    if state["last_signal"] is None:
         raise HTTPException(status_code=400, detail="No signal available")
 
-    # Calculate take-profit and stop-loss levels
-    take_profit = calculate_take_profit(last_signal["close"])
-    stop_loss = calculate_stop_loss(last_signal["close"])
+    formatted_output = format_order(state["last_signal"])
+    return formatted_output
 
-    # Format JSON output
-    formatted_output = {
-        "symbol": last_signal["symbol"],
-        "close": last_signal["close"],
-        "volume": last_signal["volume"],
-        "interval": last_signal["interval"],
-        "strategy": last_signal["strategy"],
-        "take_profit": take_profit,
-        "stop_loss": stop_loss,
-        # Add other tags for the Crypto.com exchange API
-        "type": "LIMIT",
-        "side": "BUY",
-        "price": last_signal["close"],
-        "quantity": last_signal["volume"],
-    }
 
-    return formatted_output  # FastAPI will automatically convert this into JSON
+@router.get("/order")
+def get_order():
+    if state["last_signal"] is None:
+        raise HTTPException(status_code=400, detail="No signal available")
+
+    formatted_output = format_order(state["last_signal"])
+    return formatted_output
 
 
 def calculate_take_profit(entry_price):
@@ -60,3 +49,27 @@ def calculate_take_profit(entry_price):
 def calculate_stop_loss(entry_price):
     stop_loss = entry_price - (entry_price * risk_percentage)
     return round(stop_loss, 2)
+
+
+def format_order(signal):
+    # Calculate take-profit and stop-loss levels
+    take_profit = calculate_take_profit(signal["close"])
+    stop_loss = calculate_stop_loss(signal["close"])
+
+    # Format JSON output
+    formatted_output = {
+        "symbol": signal["symbol"],
+        "close": signal["close"],
+        "volume": signal["volume"],
+        "interval": signal["interval"],
+        "strategy": signal["strategy"],
+        "take_profit": take_profit,
+        "stop_loss": stop_loss,
+        # Add other tags for the Crypto.com exchange API
+        "type": "LIMIT",
+        "side": "BUY",
+        "price": signal["close"],
+        "quantity": signal["volume"],
+    }
+
+    return formatted_output
