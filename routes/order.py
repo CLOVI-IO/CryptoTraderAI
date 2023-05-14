8b16d766aec01f09a85f97702fec6148dce86dae
cboseb <virtual.user@gmail.com>
2023-05-14 13:10:01 -0400

    Code Update

--- routes/order.py
+++ routes/order.py
@@ -1,17 +1,65 @@
 from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from shared_state import state  # Import the shared state
 import traceback  # import traceback module for detailed error logging
 
 router = APIRouter()
 
 
+# Define the nested classes for the data model
+class Order(BaseModel):
+    action: str = Field(alias="{{strategy.order.action}}")
+    contracts: str = Field(alias="{{strategy.order.contracts}}")
+    price: str = Field(alias="{{strategy.order.price}}")
+    id: str = Field(alias="{{strategy.order.id}}")
+    comment: str = Field(alias="{{strategy.order.comment}}")
+    alert_message: str = Field(alias="{{strategy.order.alert_message}}")
+
+
+class StrategyInfo(BaseModel):
+    position_size: str = Field(alias="{{strategy.position_size}}")
+    order: Order
+    market_position: str = Field(alias="{{strategy.market_position}}")
+    market_position_size: str = Field(alias="{{strategy.market_position_size}}")
+    prev_market_position: str = Field(alias="{{strategy.prev_market_position}}")
+    prev_market_position_size: str = Field(
+        alias="{{strategy.prev_market_position_size}}"
+    )
+
+
+class Plots(BaseModel):
+    plot_0: str = Field(alias="{{plot_0}}")
+    plot_1: str = Field(alias="{{plot_1}}")
+
+
+class CurrentInfo(BaseModel):
+    fire_time: str = Field(alias="{{timenow}}")
+    plots: Plots
+
+
+class BarInfo(BaseModel):
+    open: str = Field(alias="{{open}}")
+    high: str = Field(alias="{{high}}")
+    low: str = Field(alias="{{low}}")
+    close: str = Field(alias="{{close}}")
+    volume: str = Field(alias="{{volume}}")
+    time: str = Field(alias="{{time}}")
+
+
+class AlertInfo(BaseModel):
+    exchange: str = Field(alias="{{exchange}}")
+    ticker: str = Field(alias="{{ticker}}")
+    price: str = Field(alias="{{close}}")
+    volume: str = Field(alias="{{volume}}")
+    interval: str = Field(alias="{{interval}}")
+
+
+# Main Signal model
 class Signal(BaseModel):
-    symbol: str
-    close: float
-    volume: float
-    interval: str
-    strategy: str
+    alert_info: AlertInfo
+    bar_info: BarInfo
+    current_info: CurrentInfo
+    strategy_info: StrategyInfo
 
 
 @router.get("/order")
@@ -22,28 +70,26 @@ def get_order():
             raise HTTPException(status_code=400, detail="No signal available")
 
         # Retrieve the last signal from the shared state
-        last_signal = state["last_signal"]
+        last_signal = Signal(**state["last_signal"])
 
         print(f"Retrieved signal from shared state: {last_signal}")  # Debug print
 
         # Based on the strategy of the signal, define the order type and side
         order_type = "LIMIT"
-        order_side = "BUY"
-        if last_signal.get("strategy") == "SELL":
-            order_side = "SELL"
+        order_side = last_signal.strategy_info.order.action
 
         # Format JSON output
         formatted_output = {
-            "symbol": last_signal.get("symbol", "N/A"),
-            "close": last_signal.get("close", "N/A"),
-            "volume": last_signal.get("volume", "N/A"),
-            "interval": last_signal.get("interval", "N/A"),
-            "strategy": last_signal.get("strategy", "N/A"),
+            "symbol": last_signal.alert_info.ticker,
+            "close": last_signal.alert_info.price,
+            "volume": last_signal.alert_info.volume,
+            "interval": last_signal.alert_info.interval,
+            "strategy": last_signal.strategy_info.market_position,
             # Add other tags for the Crypto.com exchange API
             "type": order_type,
             "side": order_side,
-            "price": last_signal.get("close", "N/A"),
-            "quantity": last_signal.get("volume", "N/A"),
+            "price": last_signal.strategy_info.order.price,
+            "quantity": last_signal.strategy_info.position_size,
         }
 
         print(f"Formatted order: {formatted_output}")  # Debug print
---
+++ routes/tradeguard.py
@@ -0,0 +1,2 @@
+# Check the user account and protect the account and can send API command to the exchange to sell positions.
+# WEBSOCKET probably the best option
