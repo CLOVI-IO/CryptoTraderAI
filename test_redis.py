0075e37fc46fc929cbb5da3a13a8a22ceb8da662
cboseb <virtual.user@gmail.com>
2023-05-20 20:15:17 +0000

    Code Update

--- test_redis.py
+++ test_redis.py
@@ -1,7 +1,14 @@
 import os
+import redis
 
 # Get the Redis endpoint from the environment variables
 redis_endpoint = os.getenv("REDIS_ENDPOINT")
 
-# Print the Redis endpoint
-print("Redis Endpoint:", redis_endpoint)
+# Create a connection to the Redis server
+try:
+    r = redis.Redis(host=redis_endpoint, port=6379, db=0)
+    # Test the connection
+    r.ping()
+    print("Connected to Redis at", redis_endpoint)
+except redis.ConnectionError:
+    print("Failed to connect to Redis at", redis_endpoint)
