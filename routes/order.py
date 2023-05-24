ac695b21633c26f705768542f97dde18e8be049f
cboseb <virtual.user@gmail.com>
2023-05-24 02:02:13 +0000

    store the resulting order in Redis under the key "last_order"

--- routes/order.py
+++ routes/order.py
@@ -1,25 +1,12 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel, Field
-from shared_state import state  # Import the shared state
 import traceback  # import traceback module for detailed error logging
-from typing import Optional
-
-# Output json Exemple
-# {
-#     "symbol": "SOLUSDT",
-#     "close": "20.96",
-#     "volume": "256.81",
-#     "interval": "1",
-#     "strategy": "Open Long",
-#     "type": "LIMIT",
-#     "side": "BUY",
-#     "price": "20.96",
-#     "quantity": "256.81"
-# }
-
+from typing import Optional, List
+import os
+import json
+import redis
 
 router = APIRouter()
-
 
 # Define the nested classes for the data model
 class Order(BaseModel):
@@ -30,7 +17,6 @@ class Order(BaseModel):
     comment: Optional[str]
     alert_message: Optional[str]
 
-
 class StrategyInfo(BaseModel):
     position_size: Optional[str]
     order: Order
@@ -39,16 +25,13 @@ class StrategyInfo(BaseModel):
     prev_market_position: Optional[str]
     prev_market_position_size: Optional[str]
 
-
 class Plots(BaseModel):
     plot_0: Optional[str]
     plot_1: Optional[str]
 
-
 class CurrentInfo(BaseModel):
     fire_time: Optional[str]
     plots: Plots
-
 
 class BarInfo(BaseModel):
     open: Optional[str]
@@ -58,14 +41,12 @@ class BarInfo(BaseModel):
     volume: Optional[str]
     time: Optional[str]
 
-
 class AlertInfo(BaseModel):
     exchange: Optional[str]
     ticker: Optional[str]
     price: Optional[str]
     volume: Optional[str]
     interval: Optional[str]
-
 
 # Main Signal model
 class Signal(BaseModel):
@@ -74,39 +55,57 @@ class Signal(BaseModel):
     current_info: CurrentInfo
     strategy_info: StrategyInfo
 
-
 # New Payload class
 class Payload(BaseModel):
     signal: Signal
 
-
 @router.post("/order")
-def get_order(payload: Payload):
+def get_order(payload: Payload,
+              client_oid: Optional[str] = None,
+              exec_inst: Optional[List[str]] = None,
+              time_in_force: Optional[str] = None,
+              ref_price: Optional[str] = None,
+              ref_price_type: Optional[str] = None,
+              spot_margin: Optional[str] = None):
     try:
-        last_signal = payload.signal.dict()
+        REDIS_HOST = os.getenv("REDIS_HOST")
+        REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+        REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
+        r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
 
-        # Based on the strategy of the signal, define the order type and side
-        order_type = "LIMIT"
-        order_side = "BUY"
-        if last_signal["strategy_info"]["order"]["action"] == "SELL":
-            order_side = "SELL"
+        last_signal = json.loads(r.get("last_signal"))
+
+        # Extract action from the strategy_info
+        action = last_signal["strategy_info"]["order"]["action"]
+
+        # Split action into side and type
+        order_side, order_type = action.split('_')
 
         # Format JSON output
-        formatted_output = {
-            "symbol": last_signal["alert_info"]["ticker"],
+        order = {
+            "instrument_name": last_signal["alert_info"]["ticker"],
             "close": last_signal["bar_info"]["close"],
             "volume": last_signal["alert_info"]["volume"],
             "interval": last_signal["alert_info"]["interval"],
-            "strategy": last_signal["strategy_info"]["order"]["action"],
+            "strategy": action,
             "type": order_type,
             "side": order_side,
             "price": last_signal["bar_info"]["close"],
             "quantity": last_signal["alert_info"]["volume"],
         }
 
-        return formatted_output
+        # Add optional parameters if they're provided
+        if client_oid:
+            order["client_oid"] = client_oid
+        if exec_inst:
+            order["exec_inst"] = exec_inst
+        if time_in_force:
+            order["time_in_force"] = time_in_force
+        if ref_price:
+            order["ref_price"] = ref_price
+        if ref_price_type:
+            order["ref_price_type"] = ref_price_type
+        if spot_margin:
+            order["spot_margin"] = spot_margin
 
-    except Exception as e:
-        # print the traceback of the error
-        print(f"Failed to create order. Traceback: {traceback.format_exc()}")
-        raise HTTPException(status_code=500, detail=f"Failed to create order: {str(e)}")
+       
