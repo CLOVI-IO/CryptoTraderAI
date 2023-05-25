f5f60722ab5cd90032ae6d0bdd8735e63bf80efb
cboseb <virtual.user@gmail.com>
2023-05-25 18:15:09 +0000

    Code Update

--- routes/tradeguard.py
+++ routes/tradeguard.py
@@ -3,32 +3,27 @@
 from fastapi import APIRouter, HTTPException
 import os
 import json
+import time  # Add this import
 from redis_handler import RedisHandler
 from exchanges.crypto_com.private.user_balance import fetch_user_balance
 
 router = APIRouter()
 redis_handler = RedisHandler()
-TRADE_PERCENTAGE = float(
-    os.getenv("TRADE_PERCENTAGE", 5)
-)  # get trade percentage from environment variable
+TRADE_PERCENTAGE = float(os.getenv("TRADE_PERCENTAGE", 5))
 
 
 async def fetch_order_quantity(ref_price):
-    # Fetch user balance from Redis
+    # First update the user balance by fetching it from the exchange
+    await fetch_user_balance()
+
+    # Fetch updated user balance from Redis
     user_balance_data = redis_handler.redis_client.get("user_balance")
     if not user_balance_data:
         raise HTTPException(status_code=500, detail="User balance not found in Redis.")
 
     user_balance = json.loads(user_balance_data)
 
-    # Find USD account in the list of accounts
-    usd_account = None
-    for account in user_balance["result"]["accounts"]:
-        if account["currency"] == "USD":
-            usd_account = account
-            break
-
-    if not usd_account:
+    if "USD" not in user_balance["result"]:
         raise HTTPException(
             status_code=500,
             detail=f"USD not found in user balance. Balance: {user_balance}",
@@ -36,7 +31,7 @@ async def fetch_order_quantity(ref_price):
 
     # Calculating the amount available for trading
     amount_available_to_trade = (TRADE_PERCENTAGE / 100) * float(
-        usd_account["available"]
+        user_balance["result"]["USD"]["available"]
     )
     # Calculating order quantity
     order_quantity = amount_available_to_trade / float(ref_price)
@@ -47,5 +42,15 @@ async def fetch_order_quantity(ref_price):
 # New endpoint for getting order quantity
 @router.get("/order_quantity/{ref_price}")
 async def get_order_quantity(ref_price: float):
+    start_time = time.time()  # Save the start time
+
     quantity = await fetch_order_quantity(ref_price)
-    return {"quantity": quantity}
+
+    end_time = time.time()  # Save the end time
+    latency = end_time - start_time  # Calculate the difference, which is the latency
+    print(f"Endpoint latency: {latency} seconds")  # Print the latency
+
+    return {
+        "quantity": quantity,
+        "latency": latency,
+    }  # Return the latency along with the quantity
