7599570a785f260c006dfaa4f7b34dea570d31c6
cboseb <virtual.user@gmail.com>
2023-05-25 21:33:31 +0000

    Code Update

--- exchanges/crypto_com/private/user_balance.py
+++ exchanges/crypto_com/private/user_balance.py
@@ -2,21 +2,18 @@ from fastapi import APIRouter, HTTPException
 import asyncio
 import time
 import json
+import logging
 from exchanges.crypto_com.public.auth import Authentication
 from redis_handler import RedisHandler
 
 auth = Authentication()
 redis_handler = RedisHandler()
-
 router = APIRouter()
 
+logging.basicConfig(level=logging.INFO)
 
-async def fetch_user_balance():
-    # authenticate when required
-    if not auth.authenticated:
-        print("Authenticating...")
-        await auth.authenticate()
 
+async def send_user_balance_request():
     nonce = str(int(time.time() * 1000))
     method = "private/user-balance"
     id = int(nonce)
@@ -28,29 +25,51 @@ async def fetch_user_balance():
         "nonce": nonce,
     }
 
-    print("Sending request:", request)
+    logging.info("Sending request: %s", request)
     await auth.send_request(request)
+    return id, request
+
+
+async def fetch_user_balance():
+    # authenticate when required
+    if not auth.authenticated:
+        logging.info("Authenticating...")
+        await auth.authenticate()
+
+    request_id, request = await send_user_balance_request()
 
     while True:
         response = await auth.websocket.recv()
         response = json.loads(response)
-        print("Received response:", response)
+        logging.debug("Received response: %s", response)
 
-        if "id" in response and response["id"] == id:
+        if "id" in response and response["id"] == request_id:
             if "code" in response and response["code"] == 0:
                 # Store user balance in Redis
                 redis_handler.redis_client.set("user_balance", json.dumps(response))
-                print("Stored user balance in Redis.")
+                logging.info("Stored user balance in Redis.")
                 # Retrieve stored data for debugging purposes
                 user_balance_redis = redis_handler.redis_client.get("user_balance")
-                print(f"Retrieved from Redis: {user_balance_redis}")
-                return response
+                logging.debug("Retrieved from Redis: %s", user_balance_redis)
+                return {
+                    "message": "Successfully fetched user balance",
+                    "balance": response,
+                }
             else:
-                raise Exception(f"Error fetching user balance. Response: {response}")
+                logging.error(
+                    "Error fetching user balance. Request: %s, Response: %s",
+                    request,
+                    response,
+                )
+                raise Exception("Error fetching user balance")
 
-        raise Exception(
-            f"Response id does not match request id. Request id: {id}, Response: {response}"
+        logging.error(
+            "Response id does not match request id. Request id: %s, Request: %s, Response: %s",
+            request_id,
+            request,
+            response,
         )
+        raise Exception("Response id does not match request id")
 
 
 @router.post("/exchanges/crypto_com/private/user_balance")
@@ -59,12 +78,13 @@ async def get_user_balance():
         start_time = time.time()  # Save the start time
         response = await fetch_user_balance()
         end_time = time.time()  # Save the end time
-        latency = (
-            end_time - start_time
-        )  # Calculate the difference, which is the latency
-        print(f"Latency for get_user_balance: {latency} seconds")  # Print the latency
+        latency = end_time - start_time
+        logging.info(
+            "Latency for get_user_balance: %s seconds", latency
+        )  # Print the latency
         return {"response": response, "latency": latency}
     except Exception as e:
+        logging.error("Failed to get user balance. Error: %s", str(e))
         raise HTTPException(
             status_code=500, detail=f"Failed to get user balance. Error: {str(e)}"
         )
--- exchanges/crypto_com/public/auth.py
+++ exchanges/crypto_com/public/auth.py
@@ -6,9 +6,12 @@ import websockets
 import json
 import os
 import datetime
+import logging
 from dotenv import load_dotenv
 
 load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
 
 
 class Authentication:
@@ -24,70 +27,74 @@ class Authentication:
         else:
             uri = os.getenv("SANDBOX_USER_API_WEBSOCKET")
 
-        self.websocket = await websockets.connect(uri)
+        try:
+            self.websocket = await websockets.connect(uri)
+        except Exception as e:
+            logging.error(f"Failed to connect to the WebSocket: {e}")
+            raise
+
+    async def send_auth_request(self):
+        api_key = os.getenv("CRYPTO_COM_API_KEY")
+        secret_key = os.getenv("CRYPTO_COM_API_SECRET")
+        nonce = str(int(time.time() * 1000))
+        method = "public/auth"
+        id = int(nonce)
+
+        sig_payload = method + str(id) + api_key + nonce
+        sig = hmac.new(
+            secret_key.encode(), sig_payload.encode(), hashlib.sha256
+        ).hexdigest()
+
+        auth_request = {
+            "id": id,
+            "method": method,
+            "api_key": api_key,
+            "sig": sig,
+            "nonce": nonce,
+        }
+
+        logging.debug(f"Last 5 characters of the API key: {api_key[-5:]}")
+
+        await self.websocket.send(json.dumps(auth_request))
+        return id
 
     async def authenticate(self):
         while True:
             if self.websocket is None or self.websocket.closed:
                 await self.connect()
 
-            api_key = os.getenv("CRYPTO_COM_API_KEY")
-            secret_key = os.getenv("CRYPTO_COM_API_SECRET")
-            nonce = str(int(time.time() * 1000))
-            method = "public/auth"
-            id = int(nonce)
+            id = await self.send_auth_request()
+            response = await self.websocket.recv()
+            response = json.loads(response)
 
-            sig_payload = method + str(id) + api_key + nonce
-            sig = hmac.new(
-                secret_key.encode(), sig_payload.encode(), hashlib.sha256
-            ).hexdigest()
-
-            auth_request = {
-                "id": id,
-                "method": method,
-                "api_key": api_key,
-                "sig": sig,
-                "nonce": nonce,
-            }
-
-            print(f"Last 5 characters of the API key: {api_key[-5:]}")
-
-            send_time = datetime.datetime.utcnow()
-
-            try:
-                await self.websocket.send(json.dumps(auth_request))
-                response = await self.websocket.recv()
-                response = json.loads(response)
-
-                if "id" in response and response["id"] == id:
-                    receive_time = datetime.datetime.utcnow()
-                    latency = receive_time - send_time
-                    print(f"Latency: {latency.total_seconds()} seconds")
-
-                    if "code" in response:
-                        if response["code"] == 0:
-                            self.authenticated = True
-                            print("Authenticated successfully")
-                            await asyncio.sleep(
-                                60 * 5
-                            )  # sleep for 5 minutes before re-authenticating
-                        else:
-                            self.authenticated = False
-                            print(
-                                f"Authentication failed with error code: {response['code']}"
-                            )
+            if "id" in response and response["id"] == id:
+                if "code" in response:
+                    if response["code"] == 0:
+                        self.authenticated = True
+                        logging.info("Authenticated successfully")
+                        await asyncio.sleep(
+                            60 * 5
+                        )  # sleep for 5 minutes before re-authenticating
                     else:
-                        print("No 'code' field in the response")
-            except websockets.exceptions.ConnectionClosedOK:
-                print("WebSocket connection closed, reconnecting...")
+                        self.authenticated = False
+                        logging.error(
+                            f"Authentication failed with error code: {response['code']}"
+                        )
+                else:
+                    logging.error("No 'code' field in the response")
+            else:
+                logging.error(
+                    f"Response id does not match request id. Request id: {id}, Response: {response}"
+                )
 
     async def send_request(self, request: dict):
         if not self.authenticated:
             await self.authenticate()
 
-        # Added check for websocket object
-        if self.websocket is None:
-            raise Exception("Websocket is not initialized. Please check connection.")
+        if self.websocket is None or self.websocket.closed:
+            raise Exception(
+                "WebSocket is not initialized or closed. Please check the connection."
+            )
 
         await self.websocket.send(json.dumps(request))
         response = await self.websocket.recv()
