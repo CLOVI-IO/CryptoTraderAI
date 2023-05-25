e7e1dadc395970e5946c8e1bcda2d2666ece688c
cboseb <virtual.user@gmail.com>
2023-05-25 23:04:24 +0000

    Code Update

--- exchanges/crypto_com/private/user_balance.py
+++ exchanges/crypto_com/private/user_balance.py
@@ -5,15 +5,26 @@ import json
 import logging
 from exchanges.crypto_com.public.auth import Authentication
 from redis_handler import RedisHandler
+from contextvars import ContextVar
+from custom_exceptions import UserBalanceException
 
-auth = Authentication()
-redis_handler = RedisHandler()
+auth_context = ContextVar("auth", default=None)
+
+
+def get_auth():
+    auth = auth_context.get()
+    if auth is None:
+        auth = Authentication()
+        auth_context.set(auth)
+    return auth
+
+
 router = APIRouter()
 
 logging.basicConfig(level=logging.INFO)
 
 
-async def send_user_balance_request():
+async def send_user_balance_request(auth):
     nonce = str(int(time.time() * 1000))
     method = "private/user-balance"
     id = int(nonce)
@@ -31,12 +42,13 @@ async def send_user_balance_request():
 
 
 async def fetch_user_balance():
+    auth = get_auth()
     # authenticate when required
     if not auth.authenticated:
         logging.info("Authenticating...")
         await auth.authenticate()
 
-    request_id, request = await send_user_balance_request()
+    request_id, request = await send_user_balance_request(auth)
 
     while True:
         response = await auth.websocket.recv()
@@ -61,7 +73,7 @@ async def fetch_user_balance():
                     request,
                     response,
                 )
-                raise Exception("Error fetching user balance")
+                raise UserBalanceException("Error fetching user balance")
 
         logging.error(
             "Response id does not match request id. Request id: %s, Request: %s, Response: %s",
@@ -69,21 +81,15 @@ async def fetch_user_balance():
             request,
             response,
         )
-        raise Exception("Response id does not match request id")
+        raise UserBalanceException("Response id does not match request id")
 
 
 @router.post("/exchanges/crypto_com/private/user_balance")
 async def get_user_balance():
     try:
-        start_time = time.time()  # Save the start time
         response = await fetch_user_balance()
-        end_time = time.time()  # Save the end time
-        latency = end_time - start_time
-        logging.info(
-            "Latency for get_user_balance: %s seconds", latency
-        )  # Print the latency
-        return {"response": response, "latency": latency}
-    except Exception as e:
+        return {"response": response}
+    except UserBalanceException as e:
         logging.error("Failed to get user balance. Error: %s", str(e))
         raise HTTPException(
             status_code=500, detail=f"Failed to get user balance. Error: {str(e)}"
