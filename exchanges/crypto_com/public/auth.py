c262049af7b01c661dc899b28e7b4f7cab6c1f91
cboseb <virtual.user@gmail.com>
2023-05-26 01:02:07 +0000

    Code Update

--- exchanges/crypto_com/public/auth.py
+++ exchanges/crypto_com/public/auth.py
@@ -1,3 +1,5 @@
+# auth.py
+
 import time
 import hashlib
 import hmac
@@ -5,12 +7,10 @@ import asyncio
 import websockets
 import json
 import os
-import logging
+import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
-
-logging.basicConfig(level=logging.INFO)
 
 
 class Authentication:
@@ -18,109 +18,78 @@ class Authentication:
         self.websocket = None
         self.loop = asyncio.get_event_loop()
         self.authenticated = False
-        self.api_key = os.getenv("CRYPTO_COM_API_KEY")
-        self.secret_key = os.getenv("CRYPTO_COM_API_SECRET")
-        self.environment = os.getenv("ENVIRONMENT", "SANDBOX")
 
     async def connect(self):
-        uri = (
-            os.getenv("PRODUCTION_USER_API_WEBSOCKET")
-            if self.environment == "PRODUCTION"
-            else os.getenv("SANDBOX_USER_API_WEBSOCKET")
-        )
+        environment = os.getenv("ENVIRONMENT", "SANDBOX")
+        if environment == "PRODUCTION":
+            uri = os.getenv("PRODUCTION_USER_API_WEBSOCKET")
+        else:
+            uri = os.getenv("SANDBOX_USER_API_WEBSOCKET")
 
-        try:
-            self.websocket = await websockets.connect(uri)
-            logging.info("Successfully connected to the WebSocket.")
-            await asyncio.sleep(1)  # Adding a 1-second sleep after connection
-        except Exception as e:
-            logging.error(f"Failed to connect to the WebSocket: {e}")
-            raise
+        self.websocket = await websockets.connect(uri)
 
-    async def send_auth_request(self):
+    async def authenticate(self):
+        if self.websocket is None:
+            await self.connect()
+
+        api_key = os.getenv("CRYPTO_COM_API_KEY")
+        secret_key = os.getenv("CRYPTO_COM_API_SECRET")
         nonce = str(int(time.time() * 1000))
         method = "public/auth"
         id = int(nonce)
 
-        sig_payload = method + str(id) + self.api_key + nonce
+        sig_payload = method + str(id) + api_key + nonce
         sig = hmac.new(
-            self.secret_key.encode(), sig_payload.encode(), hashlib.sha256
+            secret_key.encode(), sig_payload.encode(), hashlib.sha256
         ).hexdigest()
 
         auth_request = {
             "id": id,
             "method": method,
-            "api_key": self.api_key,
+            "api_key": api_key,
             "sig": sig,
             "nonce": nonce,
         }
 
-        logging.debug(f"Last 5 characters of the API key: {self.api_key[-5:]}")
+        print(f"Last 5 characters of the API key: {api_key[-5:]}")
 
-        try:
-            await self.websocket.send(json.dumps(auth_request))
-        except Exception as e:
-            logging.error(f"Failed to send the auth request: {e}")
-            raise
-        return id
+        send_time = datetime.datetime.utcnow()
 
-    async def authenticate(self):
-        backoff = 1
+        await self.websocket.send(json.dumps(auth_request))
+
         while True:
-            if self.websocket is None or self.websocket.closed:
-                await self.connect()
-
-            id = await self.send_auth_request()
-
-            try:
-                response = await self.websocket.recv()
-                response = json.loads(response)
-            except Exception as e:
-                logging.error(f"Failed to receive the auth response: {e}")
-                continue  # Try authenticating again
+            response = await self.websocket.recv()
+            response = json.loads(response)
 
             if "id" in response and response["id"] == id:
-                if "code" in response and response["code"] == 0:
-                    self.authenticated = True
-                    logging.info("Authenticated successfully")
-                    await asyncio.sleep(
-                        60 * 5
-                    )  # sleep for 5 minutes before re-authenticating
-                    backoff = 1  # Reset backoff time
+                receive_time = datetime.datetime.utcnow()
+                latency = receive_time - send_time
+                print(f"Latency: {latency.total_seconds()} seconds")
+
+                if "code" in response:
+                    if response["code"] == 0:
+                        self.authenticated = True
+                        return {"message": "Authenticated successfully"}
+                    else:
+                        self.authenticated = False
+                        return {
+                            "message": f"Authentication failed with error code: {response['code']}"
+                        }
                 else:
-                    self.authenticated = False
-                    logging.error(
-                        f"Authentication failed with error code: {response['code']}"
-                    )
-                    if "code" in response and response["code"] == 10006:
-                        # Sleep to handle rate limit error and use exponential backoff
-                        await asyncio.sleep(backoff)
-                        backoff *= 2
-            else:
-                logging.error(
-                    f"Response id does not match request id. Request id: {id}, Response: {response}"
-                )
+                    return {"message": "No 'code' field in the response"}
 
     async def send_request(self, request: dict):
         if not self.authenticated:
             await self.authenticate()
-
-        if self.websocket.closed:
-            await self.connect()
-
-        try:
-            await self.websocket.send(json.dumps(request))
-            response = await self.websocket.recv()
-            return json.loads(response)
-        except Exception as e:
-            logging.error(f"Failed to send request: {e}")
-            raise
-
-    def run(self):
-        self.loop.run_until_complete(self.authenticate())
-        self.loop.close()
+        await self.websocket.send(json.dumps(request))
+        response = await self.websocket.recv()
+        return json.loads(response)
 
 
-if __name__ == "__main__":
-    auth = Authentication()
-    auth.run()
+# When you run python3 -m exchanges.crypto_com.public.auth,
+# it should execute the authenticate method of the Authentication
+# class and attempt to authenticate the user.
+
+#  if __name__ == "__main__":
+#    auth = Authentication()
+#    asyncio.get_event_loop().run_until_complete(auth.authenticate())
