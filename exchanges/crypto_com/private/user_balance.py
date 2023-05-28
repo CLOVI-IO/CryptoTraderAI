dcd3fbf138c0147a85bfed88b16ae8bd22f2b7e8
cboseb <virtual.user@gmail.com>
2023-05-28 19:09:19 +0000

    Code Update

--- exchanges/crypto_com/private/user_balance.py
+++ exchanges/crypto_com/private/user_balance.py
@@ -50,7 +50,7 @@ async def send_user_balance_request():
 
 
 async def fetch_user_balance(retries=3, delay=5, max_recv_attempts=3):
-    # authenticate when required
+    # Authenticate when required
     if not auth.authenticated:
         logging.info("Authenticating...")
         await auth.authenticate()
@@ -59,14 +59,17 @@ async def fetch_user_balance(retries=3, delay=5, max_recv_attempts=3):
         try:
             start_time = datetime.utcnow()
             request_id, request = await send_user_balance_request()
-            break  # if request sent successfully, break the loop
+            break  # If request sent successfully, break the loop
         except Exception as e:
             if retries == 1:
                 end_time = datetime.utcnow()
                 latency = (end_time - start_time).total_seconds()
-                raise e  # re-raise the exception after all attempts exhausted
+                logging.error(
+                    f"Error occurred. Latency: {latency}s. Exception: {str(e)}"
+                )
+                raise e  # Re-raise the exception after all attempts exhausted
             retries -= 1
-            await asyncio.sleep(delay)  # wait before next attempt
+            await asyncio.sleep(delay)  # Wait before next attempt
 
     recv_attempts = 0
     while recv_attempts < max_recv_attempts:
@@ -86,20 +89,26 @@ async def fetch_user_balance(retries=3, delay=5, max_recv_attempts=3):
             logging.error(f"Invalid JSON response: {response}")
             raise UserBalanceException("Invalid JSON response")
 
-        logging.debug("Received response: %s", response)
+        logging.debug(
+            f"Received response at {datetime.utcnow().isoformat()}: {response}"
+        )
 
         if "method" in response and response["method"] == "public/heartbeat":
             logging.info("Received heartbeat, continuing to wait for actual response.")
-            continue  # ignore the heartbeat message and keep waiting for the actual response
+            continue  # Ignore the heartbeat message and keep waiting for the actual response
 
         if "id" in response and response["id"] == request_id:
             if "code" in response and response["code"] == 0:
                 # Store user balance in Redis
                 redis_handler.redis_client.set("user_balance", json.dumps(response))
-                logging.info("Stored user balance in Redis.")
+                logging.info(
+                    f"Stored user balance in Redis at {datetime.utcnow().isoformat()}."
+                )
                 # Retrieve stored data for debugging purposes
                 user_balance_redis = redis_handler.redis_client.get("user_balance")
-                logging.debug("Retrieved from Redis: %s", user_balance_redis)
+                logging.debug(
+                    f"Retrieved from Redis at {datetime.utcnow().isoformat()}: {user_balance_redis}"
+                )
                 end_time = datetime.utcnow()
                 latency = (end_time - start_time).total_seconds()
                 return {
@@ -112,118 +121,22 @@ async def fetch_user_balance(retries=3, delay=5, max_recv_attempts=3):
                 end_time = datetime.utcnow()
                 latency = (end_time - start_time).total_seconds()
                 logging.error(
-                    "Response id does not match request id. Expected: %s, Actual: %s, Full Response: %s",
-                    request_id,
-                    response.get("id"),
-                    response,
+                    f"Response id does not match request id. Expected: {request_id}, Actual: {response.get('id')}, Full Response: {response}, Latency: {latency}s"
                 )
                 raise UserBalanceException("Response id does not match request id")
         logging.error(
-            "Response id does not match request id. Request id: %s, Request: %s, Response: %s",
-            request_id,
-            request,
-            response,
+            f"Response id does not match request id. Request id: {request_id}, Request: {request}, Response: {response}"
         )
-        # if it reached here, it means that the response id did not match the request id, which is an error
+        # If it reached here, it means that the response id did not match the request id, which is an error
         raise UserBalanceException("Response id does not match request id")
 
     logging.error(
-        "Failed to receive the expected response after %s attempts. The last response: %s",
-        recv_attempts,
-        response,
+        f"Failed to receive the expected response after {recv_attempts} attempts. The last response: {response}"
     )
-    # if it reached here, it means that the expected response was not received after all attempts
+    # If it reached here, it means that the expected response was not received after all attempts
     raise UserBalanceException(
         "Failed to receive the expected response after all attempts"
     )
-
-    if not auth.authenticated:
-        logging.info("Authenticating...")
-        await auth.authenticate()
-
-    while retries > 0:
-        try:
-            request_id, request = await send_user_balance_request()
-            break
-        except Exception as e:
-            if retries == 1:
-                raise e
-            retries -= 1
-            await asyncio.sleep(delay)
-
-    recv_attempts = 0
-    while recv_attempts < max_recv_attempts:
-        recv_attempts += 1
-        try:
-            response = await asyncio.wait_for(auth.websocket.recv(), timeout=10)
-        except asyncio.TimeoutError:
-            logging.error("Timeout error while waiting for response.")
-            raise UserBalanceException("Timeout error while waiting for response")
-        except Exception as e:
-            logging.error(f"Error while receiving response: {str(e)}")
-            raise UserBalanceException(f"Error while receiving response: {str(e)}")
-
-        try:
-            response = json.loads(response)
-        except json.JSONDecodeError:
-            logging.error(f"Invalid JSON response: {response}")
-            raise UserBalanceException("Invalid JSON response")
-
-        logging.debug("Received response: %s", response)
-
-        if "method" in response and response["method"] == "public/heartbeat":
-            logging.info("Received heartbeat, continuing to wait for actual response.")
-            continue
-
-        if "id" in response and response["id"] == request_id:
-            if "code" in response and response["code"] == 0:
-                redis_handler.redis_client.set("user_balance", json.dumps(response))
-                logging.info("Stored user balance in Redis.")
-                user_balance_redis = redis_handler.redis_client.get("user_balance")
-                logging.debug("Retrieved from Redis: %s", user_balance_redis)
-
-                # Send it to all connected WebSocket clients.
-                for websocket in connected_websockets:
-                    await websocket.send_text(json.dumps(response))
-
-                return {
-                    "message": "Successfully fetched user balance",
-                    "balance": response,
-                }
-            else:
-                logging.error(
-                    "Response id does not match request id. Expected: %s, Actual: %s, Full Response: %s",
-                    request_id,
-                    response.get("id"),
-                    response,
-                )
-                raise UserBalanceException("Response id does not match request id")
-        logging.error(
-            "Response id does not match request id. Request id: %s, Request: %s, Response: %s",
-            request_id,
-            request,
-            response,
-        )
-        raise UserBalanceException("Response id does not match request id")
-
-    logging.error(
-        "Failed to receive the expected response after %s attempts. The last response: %s",
-        recv_attempts,
-        response,
-    )
-    raise UserBalanceException(
-        "Failed to receive the expected response after all attempts"
-    )
-
-
-@router.get("/user_balance")
-async def get_user_balance(background_tasks: BackgroundTasks):
-    user_balance_redis = redis_handler.redis_client.get("user_balance")
-    if user_balance_redis is None:
-        background_tasks.add_task(fetch_user_balance)
-        return {"message": "Started fetching user balance"}
-    else:
-        return json.loads(user_balance_redis)
 
 
 @router.get("/user_balance")
