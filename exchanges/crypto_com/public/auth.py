d0495e6d08dd21a852f9f67d994034ce17ac7c4a
cboseb <virtual.user@gmail.com>
2023-05-28 15:45:17 +0000

    Added the /auth/status endpoint to get the current status of the authentication process.

--- exchanges/crypto_com/public/auth.py
+++ exchanges/crypto_com/public/auth.py
@@ -27,6 +27,8 @@ class Authentication:
         self.websocket = None
         self.authenticated = False
         self.pending_requests = {}
+        self.status = "Not started"
+        self.result = None
 
     async def connect(self):
         environment = os.getenv("ENVIRONMENT", "SANDBOX")
@@ -36,13 +38,16 @@ class Authentication:
             uri = os.getenv("SANDBOX_USER_API_WEBSOCKET")
 
         logging.debug("Trying to connect to %s", uri)
+        self.status = f"Trying to connect to {uri}"
 
         try:
             self.websocket = await websockets.connect(uri)
             logging.debug("Successfully connected to %s", uri)
+            self.status = f"Successfully connected to {uri}"
         except Exception as e:
             logging.error(f"Failed to establish connection: {e}")
             self.websocket = None
+            self.status = f"Failed to establish connection: {e}"
 
     async def authenticate(self, retries=3):
         for i in range(retries):
@@ -72,22 +77,27 @@ class Authentication:
             }
 
             logging.debug(f"Auth request: {auth_request}")
+            self.status = f"Auth request: {auth_request}"
 
             send_time = datetime.datetime.utcnow()
 
             try:
                 await self.websocket.send(json.dumps(auth_request))
                 logging.debug("Sent auth request")
+                self.status = "Sent auth request"
             except Exception as e:
                 logging.error(f"Failed to send auth request: {e}")
+                self.status = f"Failed to send auth request: {e}"
                 continue
 
             while True:
                 try:
                     response = await self.websocket.recv()
                     logging.debug(f"Received auth response: {response}")
+                    self.status = f"Received auth response: {response}"
                 except Exception as e:
                     logging.error(f"Failed to receive auth response: {e}")
+                    self.status = f"Failed to receive auth response: {e}"
                     break
 
                 response = json.loads(response)
@@ -96,31 +106,40 @@ class Authentication:
                     receive_time = datetime.datetime.utcnow()
                     latency = receive_time - send_time
                     logging.debug(f"Latency: {latency.total_seconds()} seconds")
+                    self.status = f"Latency: {latency.total_seconds()} seconds"
 
                     if "code" in response:
                         if response["code"] == 0:
                             self.authenticated = True
-                            return {"message": "Authenticated successfully"}
+                            self.result = {"message": "Authenticated successfully"}
+                            return self.result
                         else:
                             self.authenticated = False
                             logging.error(
                                 f"Authentication failed with error code: {response['code']}"
                             )
+                            self.status = f"Authentication failed with error code: {response['code']}"
                             break
                     else:
                         logging.error("No 'code' field in the response")
+                        self.status = "No 'code' field in the response"
                         break
 
         raise AuthenticationError
 
 
+auth = Authentication()
+
+
 @router.get("/auth")
-async def authenticate_route(background_tasks: BackgroundTasks):
-    auth = Authentication()
+async def auth_endpoint(background_tasks: BackgroundTasks):
     background_tasks.add_task(auth.authenticate)
-    return {"message": "Authentication initiated."}
+    return {"message": "Authentication started"}
 
 
-if __name__ == "__main__":
-    auth = Authentication()
-    asyncio.run(auth.authenticate())
+@router.get("/auth/status")
+async def auth_status():
+    return {
+        "status": auth.status,
+        "result": auth.result,
+    }
