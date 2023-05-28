e1de4ac9eceaa0ad0e6dcdca4f3c28680529cc32
cboseb <virtual.user@gmail.com>
2023-05-28 21:51:31 +0000

    updated custom_exceptions

--- custom_exceptions.py
+++ custom_exceptions.py
@@ -8,3 +8,15 @@ class UserBalanceException(Exception):
     def __init__(self, message="User balance error occurred"):
         self.message = message
         super().__init__(self.message)
+
+
+class OrderException(Exception):
+    """Exception raised for errors in the order creation.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message="Order creation error occurred"):
+        self.message = message
+        super().__init__(self.message)
