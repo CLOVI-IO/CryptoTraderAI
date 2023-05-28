4ea9b45ffc194dd2869b6745f9fd547962e880c3
cboseb <virtual.user@gmail.com>
2023-05-28 21:07:28 +0000

    Code Update

--- exchanges/crypto_com/private/create_order.py
+++ exchanges/crypto_com/private/create_order.py
@@ -1,21 +1,26 @@
-# exchanges/crypto_com/private/create_order.py
-
-from fastapi import APIRouter, HTTPException
-import traceback
-import os
-import json
+# Import required modules
+from fastapi import APIRouter, BackgroundTasks, WebSocket, HTTPException, Depends
+from datetime import datetime
 import asyncio
 import time
+import traceback
 import logging
-from typing import Optional, List
+import json
+import uuid
 from redis_handler import RedisHandler
+from typing import Optional, List
+from exceptions import OrderException
 
+# Create a new instance of APIRouter
 router = APIRouter()
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
 
-# Sample request payload
+# Set of connected websockets
+connected_websockets = set()
+
+# Sample order request
 request_sample = {
     "id": 1,
     "nonce": int(time.time() * 1000),
@@ -26,60 +31,108 @@ request_sample = {
         "type": "LIMIT",
         "price": "50000.5",
         "quantity": "0.01",
-        "client_oid": "c5f682ed-7108-4f1c-b755-972fcdca0f02",
+        "client_oid": str(uuid.uuid4()),
         "exec_inst": ["POST_ONLY"],
         "time_in_force": "FILL_OR_KILL",
     },
 }
 
+# Create an instance of RedisHandler
+redis_handler = RedisHandler()
 
-@router.post("/exchanges/crypto_com/private/create_order")
-async def create_order():
+
+async def fetch_order(order_request):
+    start_time = datetime.utcnow()
     try:
-        redis_handler = RedisHandler()
-        r = redis_handler.redis_client
-        last_signal_json = r.get("last_signal")
-        if last_signal_json is None:
-            # Handle the case where last_signal is None
-            raise Exception("No last_signal in Redis")
-        last_signal = json.loads(last_signal_json)
-
-        # Prepare the order payload
-        request_sample["params"]["instrument_name"] = last_signal["alert_info"][
-            "ticker"
-        ]
-        request_sample["params"]["side"] = (
-            last_signal["strategy_info"]["order"]["action"].split("_")[0].upper()
-        )
-        request_sample["params"]["price"] = last_signal["bar_info"]["close"]
-
-        logging.debug(f"Creating order with payload: {request_sample}")
-
-        # TODO: Call the Exchange API to create the order with the request_sample payload
-        # NOTE: This is a placeholder and will need to be replaced with the actual API call
-
-        # Placeholder response
-        response = {
-            "id": 1,
-            "method": "private/create-order",
-            "code": 0,
-            "result": {
-                "client_oid": "c5f682ed-7108-4f1c-b755-972fcdca0f02",
-                "order_id": "18342311",
-            },
-        }
-
-        logging.debug(f"Order created: {response}")
-
-        return response
-
+        await send_order_request(order_request)
+        logging.info(f"Sent order request at {datetime.utcnow().isoformat()}.")
+        # Calculate and log the latency
+        end_time = datetime.utcnow()
+        latency = (end_time - start_time).total_seconds()
+        logging.info(f"Order request latency: {latency} seconds")
     except Exception as e:
         error_message = str(e)
         if not error_message:
             error_message = repr(e)
         logging.error(
-            f"Failed to create order. Error: {error_message}. Traceback: {traceback.format_exc()}"
+            f"Failed to fetch order. Error: {error_message}. Traceback: {traceback.format_exc()}"
         )
-        raise HTTPException(
-            status_code=500, detail=f"Failed to create order: {error_message}"
-        )
+        raise OrderException("Failed to fetch order")
+
+
+async def send_order_request(order_request):
+    # Add the code to connect to the WebSocket API here
+    # Send the order_request to the WebSocket API
+    # Store the WebSocket in connected_websockets
+    pass
+
+
+async def recv_order_response(websocket, request_id):
+    recv_attempts = 0
+    start_time = datetime.utcnow()
+    while recv_attempts < 5:
+        recv_attempts += 1
+        response = await websocket.receive_json()
+
+        if "id" in response and response["id"] == request_id:
+            if "code" in response and response["code"] == 0:
+                redis_handler.redis_client.set("last_order", json.dumps(response))
+                logging.info(
+                    f"Stored order in Redis at {datetime.utcnow().isoformat()}."
+                )
+                order_redis = redis_handler.redis_client.get("last_order")
+                logging.debug(
+                    f"Retrieved from Redis at {datetime.utcnow().isoformat()}: {order_redis}"
+                )
+                end_time = datetime.utcnow()
+                latency = (end_time - start_time).total_seconds()
+                return {
+                    "message": "Successfully fetched order",
+                    "order": response,
+                    "timestamp": start_time.isoformat(),
+                    "latency": f"{latency} seconds",
+                }
+            else:
+                end_time = datetime.utcnow()
+                latency = (end_time - start_time).total_seconds()
+                logging.error(
+                    f"Response id does not match request id. Expected: {request_id}, Actual: {response.get('id')}, Full Response: {response}, Latency: {latency}s"
+                )
+                raise OrderException("Response id does not match request id")
+        else:
+            logging.info(
+                f"Response does not contain an 'id'. Full Response: {response}"
+            )
+        await asyncio.sleep(1)
+    end_time = datetime.utcnow()
+    latency = (end_time - start_time).total_seconds()
+    logging.error(
+        f"Failed to receive expected response after 5 attempts. Last Response: {response}, Latency: {latency}s"
+    )
+    raise OrderException("Failed to receive expected response after 5 attempts")
+
+
+@router.post("/orders/")
+async def create_order(
+    request: Optional[dict] = None, websocket: WebSocket = Depends(state.websocket)
+):
+    if request is None:
+        request = request_sample
+    request_id = request["id"]
+
+    await websocket.accept()
+    connected_websockets.add(websocket)
+    logging.info(
+        f"WebSocket connection established at {datetime.utcnow().isoformat()}."
+    )
+
+    try:
+        await fetch_order(request)
+        response = await recv_order_response(websocket, request_id)
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    finally:
+        connected_websockets.remove(websocket)
+        await websocket.close()
+        logging.info(f"WebSocket connection closed at {datetime.utcnow().isoformat()}.")
--- routes/order.py
+++ routes/order.py
@@ -1,17 +1,22 @@
 # order.py
+# add last order to redis
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, WebSocket, BackgroundTasks, HTTPException
+from exchanges.crypto_com.public.auth import Authentication
 import traceback
 import os
 import json
-import asyncio
 import time
 import logging
+import asyncio
+from datetime import datetime
 from typing import Optional, List
 
 from models import Payload
 from routes.tradeguard import fetch_order_quantity
 from redis_handler import RedisHandler
+from custom_exceptions import OrderException
+from starlette.websockets import WebSocketDisconnect
 
 router = APIRouter()
 
@@ -21,83 +26,146 @@ logging.basicConfig(level=logging.DEBUG)
 # Fetch trade percentage from environment variable
 TRADE_PERCENTAGE = float(os.getenv("TRADE_PERCENTAGE", 10))
 
+auth = Authentication()
+connected_websockets = set()
 
-@router.post("/order")
-async def get_order(
-    payload: Payload,
-    client_oid: Optional[str] = None,
-    exec_inst: Optional[List[str]] = None,
-    time_in_force: Optional[str] = None,
-    ref_price: Optional[str] = None,
-    ref_price_type: Optional[str] = None,
-    spot_margin: Optional[str] = None,
-):
-    start_time = time.time()
+# Create an instance of RedisHandler
+redis_handler = RedisHandler()
+
+
+@router.websocket("/ws/order")
+async def websocket_order(websocket: WebSocket):
+    await websocket.accept()
+    connected_websockets.add(websocket)
     try:
-        redis_handler = RedisHandler()
-        r = redis_handler.redis_client
+        while True:
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        connected_websockets.remove(websocket)
+
+
+async def send_order_request():
+    method = "private/create-order"
+    nonce = str(int(time.time() * 1000))
+    id = int(nonce)
+
+    request = {
+        "id": id,
+        "method": method,
+        "params": {},
+        "nonce": nonce,
+    }
+
+    logging.info(f"Sending request at {datetime.utcnow().isoformat()}: {request}")
+    await auth.send_request(method, request["params"])
+    return id, request
+
+
+async def fetch_order(retries=3, delay=5, max_recv_attempts=3):
+    # Authenticate when required
+    if not auth.authenticated:
+        logging.info("Authenticating...")
+        await auth.authenticate()
+
+    start_time = datetime.utcnow()
+    while retries > 0:
+        try:
+            request_id, request = await send_order_request()
+            break  # If request sent successfully, break the loop
+        except Exception as e:
+            if retries == 1:
+                end_time = datetime.utcnow()
+                latency = (end_time - start_time).total_seconds()
+                logging.error(
+                    f"Error occurred. Latency: {latency}s. Exception: {str(e)}"
+                )
+                raise e  # Re-raise the exception after all attempts exhausted
+            retries -= 1
+            await asyncio.sleep(delay)  # Wait before next attempt
+
+    recv_attempts = 0
+    while recv_attempts < max_recv_attempts:
+        recv_attempts += 1
+        try:
+            response = await asyncio.wait_for(auth.websocket.recv(), timeout=10)
+        except asyncio.TimeoutError:
+            logging.error("Timeout error while waiting for response.")
+            raise OrderException("Timeout error while waiting for response")
+        except Exception as e:
+            logging.error(f"Error while receiving response: {str(e)}")
+            raise OrderException(f"Error while receiving response: {str(e)}")
+
+        try:
+            response = json.loads(response)
+        except json.JSONDecodeError:
+            logging.error(f"Invalid JSON response: {response}")
+            raise OrderException("Invalid JSON response")
 
         logging.debug(
-            f"Connecting to Redis: Host - {redis_handler.REDIS_HOST}, Port - {redis_handler.REDIS_PORT}"
+            f"Received response at {datetime.utcnow().isoformat()}: {response}"
         )
 
-        last_signal = r.get("last_signal")
-        logging.debug(f"Received last_signal from Redis: {last_signal}")
+        if "method" in response and response["method"] == "public/heartbeat":
+            logging.info("Received heartbeat, continuing to wait for actual response.")
+            continue  # Ignore the heartbeat message and keep waiting for the actual response
 
-        last_signal = json.loads(last_signal)
-        action = last_signal["strategy_info"]["order"]["action"]
-        order_side, order_type = action.split("_")
+        if "id" in response and response["id"] == request_id:
+            if "code" in response and response["code"] == 0:
+                # Store user balance in Redis
+                redis_handler.redis_client.set("last_order", json.dumps(response))
+                logging.info(
+                    f"Stored order in Redis at {datetime.utcnow().isoformat()}."
+                )
+                # Retrieve stored data for debugging purposes
+                order_redis = redis_handler.redis_client.get("last_order")
+                logging.debug(
+                    f"Retrieved from Redis at {datetime.utcnow().isoformat()}: {order_redis}"
+                )
+                end_time = datetime.utcnow()
+                latency = (end_time - start_time).total_seconds()
+                return {
+                    "message": "Successfully fetched order",
+                    "order": response,
+                    "timestamp": start_time.isoformat(),
+                    "latency": f"{latency} seconds",
+                }
+            else:
+                end_time = datetime.utcnow()
+                latency = (end_time - start_time).total_seconds()
+                logging.error(
+                    f"Response id does not match request id. Expected: {request_id}, Actual: {response.get('id')}, Full Response: {response}, Latency: {latency}s"
+                )
+                raise OrderException("Response id does not match request id")
+        logging.error(
+            f"Response id does not match request id. Request id: {request_id}, Request: {request}, Response: {response}"
+        )
+        # If it reached here, it means that the response id did not match the request id, which is an error
+        raise OrderException("Response id does not match request id")
 
-        # Fetch order quantity from TradeGuard
-        amount_available_to_trade = await fetch_order_quantity(ref_price)
+    logging.error(
+        f"Failed to receive the expected response after {recv_attempts} attempts. The last response: {response}"
+    )
+    # If it reached here, it means that the expected response was not received after all attempts
+    raise OrderException("Failed to receive the expected response after all attempts")
 
-        logging.debug(f"Amount available to trade: {amount_available_to_trade}")
 
-        ref_price = last_signal["bar_info"]["close"]
-        quantity_to_buy = amount_available_to_trade / float(ref_price)
-
-        logging.debug(f"Quantity to buy: {quantity_to_buy}")
-
-        order = {
-            "instrument_name": last_signal["alert_info"]["ticker"],
-            "close": ref_price,
-            "volume": last_signal["alert_info"]["volume"],
-            "interval": last_signal["alert_info"]["interval"],
-            "strategy": action,
-            "type": order_type,
-            "side": order_side,
-            "price": ref_price,
-            "quantity": quantity_to_buy,  # use the calculated quantity
+@router.get("/order")
+async def get_order(background_tasks: BackgroundTasks):
+    start_time = datetime.utcnow()
+    order_redis = redis_handler.redis_client.get("last_order")
+    end_time = datetime.utcnow()
+    latency = (end_time - start_time).total_seconds()
+    if order_redis is None:
+        background_tasks.add_task(fetch_order)
+        return {
+            "message": "Started fetching order",
+            "timestamp": start_time.isoformat(),
+            "latency": f"{latency} seconds",
         }
-
-        if client_oid:
-            order["client_oid"] = client_oid
-        if exec_inst:
-            order["exec_inst"] = exec_inst
-        if time_in_force:
-            order["time_in_force"] = time_in_force
-        if ref_price:
-            order["ref_price"] = ref_price
-        if ref_price_type:
-            order["ref_price_type"] = ref_price_type
-        if spot_margin:
-            order["spot_margin"] = spot_margin
-
-        r.set("last_order", json.dumps(order))
-
-        end_time = time.time()
-        latency = end_time - start_time
-
-        response = {
-            "last_order": order,
-            "execution_time": end_time,
-            "latency": latency,
+    else:
+        return {
+            "message": "Successfully fetched order",
+            "order": json.loads(order_redis),
+            "timestamp": end_time.isoformat(),
+            "latency": f"{latency} seconds",
         }
-
-        logging.debug(f"Order: {order}")
-
-        return response
-
-    except Exception as e:
-        logging.error(f"Failed to create order. Traceback: {traceback.format_exc()}")
-        raise HTTPException(status_code=500, detail=f"Failed to create order: {str(e)}")
