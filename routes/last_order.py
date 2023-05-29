7b0d30580fd717d636525d75e6655c21d0e28943
cboseb <virtual.user@gmail.com>
2023-05-29 02:11:43 +0000

    Code Update

--- routes/last_order.py
+++ routes/last_order.py
@@ -16,27 +16,25 @@ router = APIRouter()
 
 
 @router.get("/last_order")
-async def get_last_order(background_tasks: BackgroundTasks):
+async def get_last_order():
     start_time = datetime.utcnow()
 
     try:
         last_order = redis_handler.redis_client.get("last_order")
         end_time = datetime.utcnow()
         latency = (end_time - start_time).total_seconds()
-        if last_order is None:
-            background_tasks.add_task(subscribe_to_last_order)
-            return {
-                "message": "Started subscribing to last order updates.",
-                "timestamp": end_time.isoformat(),
-                "latency": f"{latency} seconds",
-            }
+
+        if last_order is not None:
+            last_order = json.loads(last_order)
         else:
-            return {
-                "message": "Successfully fetched last order",
-                "last_order": json.loads(last_order),
-                "timestamp": end_time.isoformat(),
-                "latency": f"{latency} seconds",
-            }
+            last_order = "Not available."
+
+        return {
+            "message": "Fetched last order",
+            "last_order": last_order,
+            "timestamp": end_time.isoformat(),
+            "latency": f"{latency} seconds",
+        }
 
     except Exception as e:
         print(f"Failed to get last order. Error: {str(e)}")
@@ -45,20 +43,4 @@ async def get_last_order(background_tasks: BackgroundTasks):
         print(f"Timestamp: {end_time.isoformat()}, Latency: {latency} seconds")
         raise HTTPException(
             status_code=500, detail=f"Failed to get last order: {str(e)}"
-        )
-
-
-async def subscribe_to_last_order():
-    try:
-        pubsub = redis_handler.redis_client.pubsub(ignore_subscribe_messages=True)
-        pubsub.subscribe("last_order")
-        while True:
-            message = pubsub.get_message()
-            if message:
-                print(f"Received update: {message['data']}")
-    except Exception as e:
-        print(f"Failed to subscribe to last_order updates. Error: {str(e)}")
-        raise HTTPException(
-            status_code=500,
-            detail=f"Failed to subscribe to last_order updates: {str(e)}",
         )
