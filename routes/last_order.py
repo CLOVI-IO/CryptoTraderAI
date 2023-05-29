36f11d122fc65371845cc6aceb7e21cf3058277c
cboseb <virtual.user@gmail.com>
2023-05-29 01:43:22 +0000

    Code Update

--- routes/last_order.py
+++ routes/last_order.py
@@ -1,7 +1,4 @@
-# last_order.py
-
-from fastapi import APIRouter, HTTPException, Depends
-from .auth import get_auth, Authentication
+from fastapi import APIRouter, HTTPException
 import os
 import json
 import redis
@@ -10,19 +7,19 @@ router = APIRouter()
 
 
 @router.get("/last_order")
-async def get_last_order(auth: Authentication = Depends(get_auth)):
+async def get_last_order():
     try:
-        response = await auth.send_request(
-            "private/get-order-detail", params={"order_id": "your_order_id"}
-        )
-
         REDIS_HOST = os.getenv("REDIS_HOST")
         REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
         REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
         r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
 
-        r.set("last_order", json.dumps(response))
+        last_order = json.loads(r.get("last_order"))
 
-        return response
+        return last_order
+
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        print(f"Failed to get last order. Error: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Failed to get last order: {str(e)}"
+        )
