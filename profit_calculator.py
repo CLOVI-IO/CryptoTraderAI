d4a4c5819904107df0a52dc2c31c5112093607ad
cboseb <virtual.user@gmail.com>
2023-05-31 02:38:26 +0000

    Code Update

--- profit_calculator.py
+++ profit_calculator.py
@@ -2,15 +2,16 @@ import pandas as pd
 import random
 
 # Variables
-initial_amount = 100.0  # initial amount to start with
+initial_amount = 300.0  # initial amount to start with
 transaction_percentage = 5.0 / 100  # percentage of amount used per transaction
-profit_percentage = 3.0 / 100  # average profit percentage per transaction
+profit_percentage = 5.0 / 100  # average profit percentage per transaction
 fee_percentage = 0.0750 / 100  # fee per transaction
 transactions_per_day = 20  # number of transactions per day
 goal_amount = 10000.0  # goal amount to reach
 days_in_year = 365  # number of days in a year
-success_rate = 82.0 / 100  # success rate per transaction
-loss_percentage = 1.5 / 100  # loss percentage for unsuccessful transactions
+success_rate = 50.0 / 100  # success rate per transaction
+loss_percentage = 3 / 100  # loss percentage for unsuccessful transactions
+average_days_in_month = 30.44  # average number of days in a month
 
 
 # Function to calculate the transaction
@@ -40,6 +41,9 @@ for i in range(1, 101):
 
 # Create the dataframe and print it
 df = pd.DataFrame(data)
+pd.options.display.float_format = (
+    "${:,.2f}".format
+)  # Set the decimal format for 'Amount'
 print(df)
 
 # Calculate the number of transactions to reach the goal
@@ -50,6 +54,7 @@ while current_amount < goal_amount:
     count += 1
 
 days_to_reach_goal = count / transactions_per_day
+months_to_reach_goal = days_to_reach_goal / average_days_in_month
 
 # Calculate the estimated amount after 6 months
 transactions_in_half_year = transactions_per_day * (days_in_year // 2)
@@ -65,7 +70,22 @@ for _ in range(transactions_in_year):
     current_amount = calculate_transaction(current_amount)
 estimated_amount_12_months = current_amount
 
-print(f"\nIt will take {count} transactions to reach ${goal_amount}.")
-print(f"Estimated number of days to reach the goal: {days_to_reach_goal}")
-print(f"Estimated amount after 6 months: ${estimated_amount_6_months}")
-print(f"Estimated amount after 12 months: ${estimated_amount_12_months}")
+results_df = pd.DataFrame(
+    {
+        "Description": [
+            "Transactions to reach goal",
+            "Days to reach goal",
+            "Months to reach goal",
+            "Estimated amount after 6 months",
+            "Estimated amount after 12 months",
+        ],
+        "Value": [
+            count,
+            "{:.2f}".format(days_to_reach_goal),
+            "{:.2f}".format(months_to_reach_goal),
+            "${:,.2f}".format(estimated_amount_6_months),
+            "${:,.2f}".format(estimated_amount_12_months),
+        ],
+    }
+)
+print("\n", results_df)
--- routes/order.py
+++ routes/order.py
@@ -13,7 +13,6 @@ from datetime import datetime
 from typing import Optional, List
 
 from models import Payload
-from routes.tradeguard import fetch_order_quantity
 from redis_handler import RedisHandler
 from custom_exceptions import OrderException
 from starlette.websockets import WebSocketDisconnect
@@ -35,6 +34,7 @@ connected_websockets = set()
 redis_handler = RedisHandler()
 
 
+# WebSocket endpoint
 @router.websocket("/ws/order")
 async def websocket_order(websocket: WebSocket):
     await websocket.accept()
@@ -46,6 +46,7 @@ async def websocket_order(websocket: WebSocket):
         connected_websockets.remove(websocket)
 
 
+# Sends an order request
 async def send_order_request():
     method = "private/create-order"
     nonce = str(int(time.time() * 1000))
@@ -54,7 +55,9 @@ async def send_order_request():
     request = {
         "id": id,
         "method": method,
-        "params": {},
+        "params": {
+            "quantity": 0.01,  # Hardcoded for now
+        },
         "nonce": nonce,
     }
 
@@ -63,115 +66,36 @@ async def send_order_request():
     return id, request
 
 
-async def fetch_order(retries=3, delay=5, max_recv_attempts=3):
+# Fetches the order
+async def fetch_order():
     # Authenticate when required
     if not auth.authenticated:
         logging.info("Authenticating...")
         await auth.authenticate()
 
-    start_time = datetime.utcnow()
-    while retries > 0:
-        try:
-            request_id, request = await send_order_request()
-            break  # If request sent successfully, break the loop
-        except Exception as e:
-            if retries == 1:
-                end_time = datetime.utcnow()
-                latency = (end_time - start_time).total_seconds()
-                logging.error(
-                    f"Error occurred. Latency: {latency}s. Exception: {str(e)}"
-                )
-                raise e  # Re-raise the exception after all attempts exhausted
-            retries -= 1
-            await asyncio.sleep(delay)  # Wait before next attempt
+    request_id, request = await send_order_request()
 
-    recv_attempts = 0
-    while recv_attempts < max_recv_attempts:
-        recv_attempts += 1
-        try:
-            response = await asyncio.wait_for(auth.websocket.recv(), timeout=10)
-        except asyncio.TimeoutError:
-            logging.error("Timeout error while waiting for response.")
-            raise OrderException("Timeout error while waiting for response")
-        except Exception as e:
-            logging.error(f"Error while receiving response: {str(e)}")
-            raise OrderException(f"Error while receiving response: {str(e)}")
+    response = await auth.websocket.recv()
 
-        try:
-            response = json.loads(response)
-        except json.JSONDecodeError:
-            logging.error(f"Invalid JSON response: {response}")
-            raise OrderException("Invalid JSON response")
+    response = json.loads(response)
 
-        logging.debug(
-            f"Received response at {datetime.utcnow().isoformat()}: {response}"
-        )
+    logging.debug(f"Received response at {datetime.utcnow().isoformat()}: {response}")
 
-        if "method" in response and response["method"] == "public/heartbeat":
-            logging.info("Received heartbeat, continuing to wait for actual response.")
-            continue  # Ignore the heartbeat message and keep waiting for the actual response
-
-        if "id" in response and response["id"] == request_id:
-            if "code" in response and response["code"] == 0:
-                # Store user balance in Redis
-                redis_handler.redis_client.set("last_order", json.dumps(response))
-                logging.info(
-                    f"Stored order in Redis at {datetime.utcnow().isoformat()}."
-                )
-                # Retrieve stored data for debugging purposes
-                order_redis = redis_handler.redis_client.get("last_order")
-                logging.debug(
-                    f"Retrieved from Redis at {datetime.utcnow().isoformat()}: {order_redis}"
-                )
-                end_time = datetime.utcnow()
-                latency = (end_time - start_time).total_seconds()
-                return {
-                    "message": "Successfully fetched order",
-                    "order": response,
-                    "timestamp": start_time.isoformat(),
-                    "latency": f"{latency} seconds",
-                }
-            else:
-                end_time = datetime.utcnow()
-                latency = (end_time - start_time).total_seconds()
-                logging.error(
-                    f"Response id does not match request id. Expected: {request_id}, Actual: {response.get('id')}, Full Response: {response}, Latency: {latency}s"
-                )
-                raise OrderException("Response id does not match request id")
-        logging.error(
-            f"Response id does not match request id. Request id: {request_id}, Request: {request}, Response: {response}"
-        )
-        # If it reached here, it means that the response id did not match the request id, which is an error
-        raise OrderException("Response id does not match request id")
-
-    logging.error(
-        f"Failed to receive the expected response after {recv_attempts} attempts. The last response: {response}"
-    )
-    # If it reached here, it means that the expected response was not received after all attempts
-    raise OrderException("Failed to receive the expected response after all attempts")
-
-
-@router.get("/order")
-async def get_order(background_tasks: BackgroundTasks):
-    start_time = datetime.utcnow()
-    order_redis = redis_handler.redis_client.get("last_order")
-    end_time = datetime.utcnow()
-    latency = (end_time - start_time).total_seconds()
-    if order_redis is None:
-        background_tasks.add_task(fetch_order)
-        return {
-            "message": "Started fetching order",
-            "order": json.load(order_redis),
-            "timestamp": start_time.isoformat(),
-            "latency": f"{latency} seconds",
-        }
+    if "id" in response and response["id"] == request_id:
+        if "code" in response and response["code"] == 0:
+            # Store user balance in Redis
+            redis_handler.redis_client.set("last_order", json.dumps(response))
+            logging.info(f"Stored order in Redis at {datetime.utcnow().isoformat()}.")
+            # Retrieve stored data for debugging purposes
+            order_redis = redis_handler.redis_client.get("last_order")
+            logging.debug(
+                f"Retrieved from Redis at {datetime.utcnow().isoformat()}: {order_redis}"
+            )
+            return {"message": "Successfully fetched order", "order": response}
+        else:
+            raise OrderException("Response id does not match request id")
     else:
-        return {
-            "message": "Successfully fetched order",
-            "order": json.loads(order_redis),
-            "timestamp": end_time.isoformat(),
-            "latency": f"{latency} seconds",
-        }
+        raise OrderException("Response id does not match request id")
 
 
 # This function listens to changes in the last_signal key and triggers the fetch_order function when the key changes
