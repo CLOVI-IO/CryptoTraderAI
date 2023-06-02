552a85c4f20ec12b616a642546f6514870c0ccdd
cboseb <virtual.user@gmail.com>
2023-06-02 00:16:47 +0000

    Code Update

--- venv/lib/python3.11/site-packages/aioredis/compat.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if sys.version_info >= (3, 8):
-    from typing import Protocol, TypedDict
-else:
-    from typing_extensions import Protocol, TypedDict
-
-__all__ = ("Protocol", "TypedDict")
