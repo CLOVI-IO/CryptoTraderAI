12e88ba5955c7a9507aec2046977da3b954d24bf
cboseb <virtual.user@gmail.com>
2023-06-03 04:04:43 +0000

    Code Update

--- routes/order.py
+++ routes/order.py
@@ -37,7 +37,7 @@ def connect_to_redis():
 
 redis_client = connect_to_redis()
 
-@router.websocket("/ws/order")
+@router.websocket("ws://ai.clovi.io/ws/order")
 async def websocket_order(websocket: WebSocket, background_tasks: BackgroundTasks):
     await websocket.accept()
     logging.info("WebSocket accepted")
