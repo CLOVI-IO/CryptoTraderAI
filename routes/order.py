04006cb3cd40b874e79d8ebdf2beda3b08948fbc
cboseb <virtual.user@gmail.com>
2023-06-03 23:31:34 +0000

    Code Update

--- routes/order.py
+++ routes/order.py
@@ -3,12 +3,10 @@ from starlette.websockets import WebSocketDisconnect
 import os
 import json
 import time
-import aioredis
+import redis
 import logging
 from models import Payload
 from exchanges.crypto_com.public.auth import get_auth
-from aioredis.pubsub import Receiver
-from asyncio import create_task
 
 app = FastAPI() # if this is your main app. Otherwise, this would be in your main app file
 
@@ -26,18 +24,7 @@ REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
 
-# Initialize a pubsub Receiver
-mpsc = Receiver()
-
-redis_pool = None
-
-@app.on_event("startup")
-async def startup():
-    global redis_pool
-    # Asynchronously create a Redis connection pool
-    redis_pool = await aioredis.create_pool(
-        f'redis://{REDIS_HOST}:{REDIS_PORT}', password=REDIS_PASSWORD,
-        minsize=5, maxsize=10)
+redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
 
 @router.websocket("/ws/order")
 async def websocket_order(websocket: WebSocket):
@@ -45,29 +32,27 @@ async def websocket_order(websocket: WebSocket):
     logging.info("WebSocket accepted")
     connected_websockets.add(websocket)
 
-    # Use the Redis connection pool to get a connection
-    with await redis_pool as conn:
-        # Subscribe to 'last_signal' channel
-        await conn.execute('subscribe', mpsc.channel('last_signal'))
-        logging.info("Subscribed to 'last_signal' channel")
-        
-        try:
-            # Asynchronously iterate over messages in the channel
-            async for channel, message in mpsc.iter():
-                last_signal = Payload(**json.loads(message))
+    pubsub = redis_client.pubsub()
+    pubsub.subscribe("last_signal")
+    logging.info("Subscribed to 'last_signal' channel")  
+
+    try:
+        while True:
+            message = pubsub.get_message()
+            if message and message["type"] == "message":
+                last_signal = Payload(**json.loads(message["data"]))
                 logging.info(f"Received last_signal from Redis channel: {last_signal}")  
                 await send_to_order_endpoint(last_signal)  # Send data to create_order endpoint
                 await websocket.send_text(json.dumps(last_signal.dict()))  # Sending as JSON string
                 logging.debug(f"Sent signal to client: {last_signal}")
-        except WebSocketDisconnect:
-            logging.error("WebSocket disconnected.")
-            connected_websockets.remove(websocket)
-        except Exception as e:
-            logging.error(f"Unexpected error: {str(e)}")
-        finally:
-            # Unsubscribe from 'last_signal' channel
-            await conn.execute('unsubscribe', 'last_signal')
-            logging.info("Unsubscribed from 'last_signal' channel")
+    except WebSocketDisconnect:
+        logging.error("WebSocket disconnected.")
+        connected_websockets.remove(websocket)
+    except Exception as e:
+        logging.error(f"Unexpected error: {str(e)}")
+    finally:
+        pubsub.unsubscribe("last_signal")  
+        logging.info("Unsubscribed from 'last_signal' channel")
 
 async def send_to_order_endpoint(data: Payload):
     # replace with your actual create_order endpoint
@@ -82,4 +67,4 @@ async def send_to_order_endpoint(data: Payload):
     except Exception as e:
         logging.error(f"Error sending data to order endpoint: {str(e)}")
 
-logging.info("Order endpoint ready")
+logging.info("Order endpoint ready") 
