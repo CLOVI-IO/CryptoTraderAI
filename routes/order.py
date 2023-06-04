bdb0c645be0ffceb258e7f74da6de8ee8f8d4d2f
cboseb <virtual.user@gmail.com>
2023-06-04 01:58:22 +0000

    Code Update

--- routes/order.py
+++ routes/order.py
@@ -3,28 +3,21 @@ from starlette.websockets import WebSocketDisconnect
 import os
 import json
 import time
-import redis
 import logging
 from models import Payload
-from exchanges.crypto_com.public.auth import get_auth
+from redis_handler import RedisHandler  # Add this line
 
-app = FastAPI() # if this is your main app. Otherwise, this would be in your main app file
+app = FastAPI()  # if this is your main app. Otherwise, this would be in your main app file
 
 router = APIRouter()
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
 
-# Get the singleton instance of the Authentication class.
-auth = get_auth()
-
 connected_websockets = set()
 
-REDIS_HOST = os.getenv("REDIS_HOST")
-REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
-
-redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
+redis_handler = RedisHandler()  # Create RedisHandler instance
+redis_client = redis_handler.redis_client  # Access redis client from RedisHandler
 
 @router.websocket("/ws/order")
 async def websocket_order(websocket: WebSocket):
@@ -42,7 +35,6 @@ async def websocket_order(websocket: WebSocket):
             if message and message["type"] == "message":
                 last_signal = Payload(**json.loads(message["data"]))
                 logging.info(f"Order: Received last_signal from Redis channel: {last_signal}")  
-                await send_to_order_endpoint(last_signal)  # Send data to create_order endpoint
                 await websocket.send_text(json.dumps(last_signal.dict()))  # Sending as JSON string
                 logging.debug(f"Order: Sent signal to client: {last_signal}")
     except WebSocketDisconnect:
@@ -54,17 +46,4 @@ async def websocket_order(websocket: WebSocket):
         pubsub.unsubscribe("last_signal")  
         logging.info("Order: Unsubscribed from 'last_signal' channel")
 
-async def send_to_order_endpoint(data: Payload):
-    # replace with your actual create_order endpoint
-    url = "http://your_url_here/create_order"
-    try:
-        response = await auth.session.post(url, json=data.dict())
-        response.raise_for_status()
-        if response.status_code == 200:
-            logging.debug(f"Order: Successfully sent data to order endpoint. Response: {response.json()}")
-        else:
-            logging.error(f"Order: Failed to send data to order endpoint. Status code: {response.status_code}")
-    except Exception as e:
-        logging.error(f"Order: Error sending data to order endpoint: {str(e)}")
-
-logging.info(":: Order endpoint ready ::") 
+logging.info(":: Order endpoint ready ::")
