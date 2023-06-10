2fa9ffbb29220a88ef31ce813b8d003240d73177
cboseb <virtual.user@gmail.com>
2023-06-10 21:08:38 +0000

    Code Update

--- redis_handler.py
+++ redis_handler.py
@@ -1,6 +1,8 @@
 import os
-from rediscluster import RedisCluster
+import redis
 import logging
+from fastapi.responses import JSONResponse
+from rediscluster import RedisCluster
 
 
 class RedisHandler:
@@ -17,31 +19,32 @@ class RedisHandler:
         logging.debug(f"REDIS_PASSWORD={self.REDIS_PASSWORD}")
 
         try:
-            startup_nodes = [{"host": self.REDIS_HOST, "port": str(self.REDIS_PORT)}]
-
+            startup_nodes = [{"host": self.REDIS_HOST, "port": self.REDIS_PORT}]
             # Only use the password if it's provided
             if self.REDIS_PASSWORD:
-                self.redis_client = RedisCluster.RedisCluster(
+                self.redis_client = RedisCluster(
                     startup_nodes=startup_nodes,
-                    decode_responses=True,
-                    skip_full_coverage_check=True,
                     password=self.REDIS_PASSWORD,
+                    decode_responses=True,
                 )
             else:
-                self.redis_client = RedisCluster.RedisCluster(
+                self.redis_client = RedisCluster(
                     startup_nodes=startup_nodes,
                     decode_responses=True,
-                    skip_full_coverage_check=True,
                 )
 
-            logging.debug("Successfully created RedisCluster.RedisCluster instance")
+            logging.debug("Successfully created redis.RedisCluster instance")
 
             # Try a simple operation to check if the connection is established
             if self.redis_client.ping():
                 logging.debug("Successfully connected to Redis")
             else:
                 logging.error("Failed to connect to Redis")
-        except RedisCluster.exceptions.ClusterDownError as e:
-            logging.error(f"Redis cluster error: {e}")
+        except redis.exceptions.ConnectionError as e:
+            logging.error(f"Redis connection error: {e}")
+            raise JSONResponse(
+                content={"detail": f"Redis connection error: {e}"}, status_code=500
+            )
         except Exception as e:
             logging.error(f"Error: {e}")
+            raise JSONResponse(content={"detail": f"Error: {e}"}, status_code=500)
