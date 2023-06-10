6926177a203e7ab174050ab881dd013188afd610
cboseb <virtual.user@gmail.com>
2023-06-10 14:36:17 +0000

    Code Update

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # main.py
 
 from fastapi import FastAPI
-
 from routes import webhook, viewsignal, order, exchange
+from exchanges.crypto_com.private import user_balance
 
-#from routes.order import router as order  # Import the WebSocket router
+# from routes.order import router as order  # Import the WebSocket router
 
 app = FastAPI()
 
@@ -16,10 +16,11 @@ app.state.last_signal = None
 def hello_world():
     return {"message": "Hello, World!"}
 
+
 # Include the route endpoints from other files
 app.include_router(webhook.router)
 app.include_router(viewsignal.router)
 app.include_router(order.router)
 app.include_router(exchange.router)
-
+app.include_router(user_balance.router)
 # end of main.py
