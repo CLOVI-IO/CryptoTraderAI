3674c59fb59ab1b27ce4e004f50f0e52787e6a7a
cboseb <virtual.user@gmail.com>
2023-06-24 22:26:18 +0000

    Code Update

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def hello_world():
 # Include the route endpoints from other files
 app.include_router(auth.router)
 app.include_router(webhook.router)
-app.include_router(viewsignal.router)
-app.include_router(order.router)
-app.include_router(last_order.router)
-app.include_router(exchange.router)
-app.include_router(user_balance.router)
+# app.include_router(viewsignal.router)
+# app.include_router(order.router)
+# app.include_router(last_order.router)
+# app.include_router(exchange.router)
+# app.include_router(user_balance.router)
